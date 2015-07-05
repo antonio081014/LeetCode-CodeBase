e01cff60568f946ae2928e3acfded9fc25b7c808
Antonio081014 <antonio081014@users.noreply.github.com>
2015-07-05 10:49:10 -0700

    Update Trapping_Rain_Water.java

--- Trapping_Rain_Water.java
+++ Trapping_Rain_Water.java
@@ -1,6 +1,11 @@
 /**
  * Problem: https://leetcode.com/problems/trapping-rain-water/
  * 
+ * To find the trapped water at position i, we need to find the maximum value of the left elements of i and right elements of i. 
+ * Assuming they are maxLeft[i] and maxRight[i]. 
+ * The trapped water is min(maxLeft[i], maxRight[i]) – height[i] (if this value is larger than 0).
+ * 
+ * Ref: http://www.lifeincode.net/programming/leetcode-trapping-rain-water-java/
  */
 public class Solution {
     public int trap(int[] height) {
