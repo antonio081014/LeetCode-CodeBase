107b64c0168f68602351b23ebcfcc995f1bbea41
Antonio081014 <antonio081014@users.noreply.github.com>
2015-07-14 10:45:37 -0700

    Create Kth_Largest_Element_in_an_Array.java

---
+++ Kth_Largest_Element_in_an_Array.java
@@ -0,0 +1,53 @@
+/**
+ *
+ * O(N) best case / O(N^2) worst case running time + O(1) memory
+ * The smart approach for this problem is to use the selection algorithm 
+ * (based on the partion method - the same one as used in quicksort).
+ * 
+ * https://leetcode.com/discuss/36966/solution-explained
+ */
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if(j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    private int partition(int[] a, int lo, int hi) {
+
+        int i = lo;
+        int j = hi + 1;
+        while(true) {
+            while(i < hi && less(a[++i], a[lo]));
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private boolean less(int v, int w) {
+        return v < w;
+    }
+}
