0e291d322a668fe6303f8d529aeb16e1ff083d94
Antonio081014 <antonio081014@users.noreply.github.com>
2015-07-26 23:12:52 -0700

    Create Delete_Node_in_a_Linked_List.java

---
+++ Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void deleteNode(ListNode node) {
+        if(node.next == null) node = null;
+        else {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
