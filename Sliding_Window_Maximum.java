fb5ee0393f7c85e520ceed6d300332a2024e94b1
Antonio081014 <antonio081014@users.noreply.github.com>
2015-09-07 16:33:00 -0700

    Update Sliding_Window_Maximum.java

--- Sliding_Window_Maximum.java
+++ Sliding_Window_Maximum.java
@@ -2,6 +2,10 @@
  * Problem: https://leetcode.com/problems/sliding-window-maximum/
  * 
  * Using Deque with Queue implementation.
+ * 
+ * There is another solution for this problem, using a prioirty queue, 
+ * which will have a pair of value and position in the queue.
+ * Each time before adding any elements, remove the top elements if they are out of the window.
  */
 
 public class Solution {
@@ -11,13 +15,17 @@ public class Solution {
         int[] ret = new int[nums.length - w + 1];
         Deque<Integer> q = new LinkedList<Integer>();
         for(int i=0; i<w; i++) {
+            // Only keep the largest element in the queue for the first w elements.
             while(!q.isEmpty() && nums[q.getLast()] < nums[i]) q.removeLast();
             q.addLast(i);
         }
         
         for(int i=w; i<nums.length; i++) {
             ret[i-w] = nums[q.getFirst()];
+            // Only keep the elements larger than current element, 
+            // since these elements have no chance to be the one.
             while(!q.isEmpty() && nums[q.getLast()] < nums[i]) q.removeLast();
+            // Remove all the elements which will be out of the window slide.
             while(!q.isEmpty() && q.getFirst() <= i-w ) q.removeFirst();
             q.addLast(i);
         }
