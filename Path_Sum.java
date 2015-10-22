c308f8e9a78e7f77067e0013593ff979ea698c44
Antonio081014 <antonio081014@users.noreply.github.com>
2015-10-22 15:54:48 -0700

    Update Path_Sum.java

--- Path_Sum.java
+++ Path_Sum.java
@@ -11,9 +11,22 @@
  */
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        return check(root, sum);
+    }
+    
+    private boolean check(TreeNode root, int sum){
         if(root == null && sum == 0) return true;
         else if(root == null) return false;
-        
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        if(root.left != null && root.right == null) {
+            return check(root.left, sum - root.val);
+        }
+        if(root.left == null && root.right != null) {
+            return check(root.right, sum - root.val);
+        }
+        if(root.left != null && root.right != null) {
+            return check(root.left, sum - root.val) || check(root.right, sum - root.val);
+        }
+        return sum == root.val;
     }
 }
