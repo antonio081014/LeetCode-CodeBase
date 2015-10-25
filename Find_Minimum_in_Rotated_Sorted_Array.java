4b55ecdf6b5ff5b2f2a8036387bd3fac2a26c139
Antonio081014 <antonio081014@users.noreply.github.com>
2015-10-25 16:46:05 -0700

    Create Find_Minimum_in_Rotated_Sorted_Array.java

---
+++ Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,11 @@
+/**
+ * Problem Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ */
+public class Solution {
+    public int findMin(int[] nums) {
+        for(int i=0; i+1<nums.length; i++) {
+            if(nums[i] > nums[i+1]) return nums[i+1];
+        }
+        return nums[0];
+    }
+}
