8c576dfbd38c99483e873b267bbd6870bf2a0b62
Antonio081014 <antonio081014@users.noreply.github.com>
2015-11-17 16:57:40 -0800

    Update Jump_Game_II.java

--- Jump_Game_II.java
+++ Jump_Game_II.java
@@ -19,6 +19,12 @@ class Node implements Comparable<Node>{
     }
 }
 public class Solution {
+    /*
+     * We use "last" to keep track of the maximum distance that has been reached
+     * by using the minimum steps "ret", whereas "curr" is the maximum distance
+     * that can be reached by using "ret+1" steps. Thus,
+     * curr = max(i+A[i]) where 0 <= i <= last.
+     */
     public int jump(int[] nums) {
         int ret = 0;
         int last = 0;
