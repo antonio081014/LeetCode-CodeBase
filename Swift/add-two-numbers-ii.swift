fab5566c6ea6d0640f6179992ba24dbc38c4123c
Antonio081014 <antonio081014@antonio081014.com>
2016-12-29 10:56:24 -0800

    add-two-numbers-ii

--- Swift/add-two-numbers-ii.swift
+++ Swift/add-two-numbers-ii.swift
@@ -44,4 +44,52 @@ class Solution {
         }
         return head.next
     }
+    
+    // Less space but the same time complexity.
+    // Both are linear. O(N), N is the max length of two.
+    func addTwoNumbers(_ ll1: ListNode?, _ ll2: ListNode?) -> ListNode? {
+        var l1 = ll1
+        var c1 = 0
+        while l1 != nil {
+            l1 = l1!.next
+            c1 += 1
+        }
+        var l2 = ll2
+        var c2 = 0
+        while l2 != nil {
+            l2 = l2!.next
+            c2 += 1
+        }
+        var cmax = max(c1, c2)
+        l1 = ll1
+        l2 = ll2
+        var stack = [Int]()
+        while cmax>0  {
+            var sum = 0
+            if cmax==c1, c1>0 {
+                sum += l1!.val
+                c1-=1
+                l1 = l1!.next
+            }
+            if cmax==c2, c2>0 {
+                sum += l2!.val
+                c2-=1
+                l2 = l2!.next
+            }
+            cmax -= 1
+            stack += [sum]
+        }
+        var head = ListNode(0)
+        var flag = 0
+        while !stack.isEmpty || flag>0 {
+            if stack.count > 0 {
+                flag += stack.popLast()!
+            }
+            var node = ListNode(flag%10)
+            node.next = head.next
+            head.next = node
+            flag /= 10
+        }
+        return head.next
+    }
 }
