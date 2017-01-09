56a9aa521df13b0a48d376b06ca63d998243f9ff
Antonio081014 <antonio081014@antonio081014.com>
2017-01-09 15:36:04 -0800

    Add additional solution to arithmetic-slices

--- Swift/arithmetic-slices.swift
+++ Swift/arithmetic-slices.swift
@@ -4,7 +4,22 @@
  * 
  */ 
 class Solution {
+    // Dynamic Programming solution.
     func numberOfArithmeticSlices(_ A: [Int]) -> Int {
+        let n = A.count
+        guard n >= 3 else { return 0 }
+        var mark = Array(repeating: 0, count: n)
+        var total = 0
+        for i in 2..<n {
+            if A[i]-A[i-1] == A[i-1]-A[i-2] {
+                mark[i] = mark[i-1] + 1
+            }
+            total += mark[i]
+        }
+        return total
+    }
+    // Simplified DP solution.
+    func numberOfArithmeticSlices2(_ A: [Int]) -> Int {
         let n = A.count
         guard n >= 3 else { return 0 }
         var c = 0
