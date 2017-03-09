bf928a65c8df98e0417a4934a1c9ce013ed337a7
Antonio081014 <antonio081014@users.noreply.github.com>
2017-03-08 16:37:31 -0800

    Add additional solution.

--- Swift/relative-ranks.swift
+++ Swift/relative-ranks.swift
@@ -4,6 +4,7 @@
  * 
  */ 
 class Solution {
+    // Fulfill with Collection reduce and map.
     func findRelativeRanks(_ nums: [Int]) -> [String] {
         let n = nums.count
         let indexedList: [(Int, Int)] = (0..<n).reduce([(Int, Int)]()) {
@@ -22,4 +23,20 @@ class Solution {
             }.sorted(by: { $0.0 < $1.0 })
         return textedList.map() { $0.1 }
     }
+    
+    // Fulfill with Collection map only.
+    func findRelativeRanks(_ nums: [Int]) -> [String] {
+        let n = nums.count
+        let indexedList: [(Int, Int)] = (0..<n).map() { ($0, nums[$0]) }.sorted(by: {$0.1 > $1.1})
+        let textedList: [(Int, String)] = (0..<n).map() { 
+            switch $0 {
+                case 0: return (indexedList[$0].0, "Gold Medal")
+                case 1: return (indexedList[$0].0, "Silver Medal")
+                case 2: return (indexedList[$0].0, "Bronze Medal")
+                default: return (indexedList[$0].0, "\($0 + 1)")
+            }
+        }.sorted(by: { $0.0 < $1.0 })
+        return textedList.map() { $0.1 }
+    }
 }
+
