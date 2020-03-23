8871592ed5534cf1057e63fb7001bf622abdc568
Antonio081014 <antonio081014@antonio081014.com>
2020-03-23 13:40:04 -0700

    Add Add comments to problem 1373. Maximum Sum BST in Binary Tree.swift

--- Swift/maximum-sum-bst-in-binary-tree.swift
+++ Swift/maximum-sum-bst-in-binary-tree.swift
@@ -4,11 +4,6 @@
  * 
  */ 
 /**
- * https://leetcode.com/problems/maximum-sum-bst-in-binary-tree/
- * 
- * 
- */ 
-/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     public var val: Int
@@ -21,8 +16,14 @@
  *     }
  * }
  */
+
+ /**
+  *
+  *  
+  */
 class Solution {
     func maxSumBST(_ root: TreeNode?) -> Int {
+        // Init.
         self.maxSum = 0
         
         guard let root = root else { return self.maxSum }
@@ -32,8 +33,25 @@ class Solution {
         return self.maxSum
     }
     
+    /// The max sum value of a valid BST or sub BST.
     private var maxSum = 0
     
+    /// Go through every node of tree with DFS.
+    /// Check on the validation of both left subtree and right subtree while calculating the sum of each subtrees.
+    /// If both of subtrees are valid and current tree is also valid, compare the maxSum with sum for current tree, rooted from `root`
+    /// Otherwise, current tree should contribute nothing for the sum of this tree, which might be the subtree of another node.
+    /// 
+    /// - Parameters:
+    ///     - root: Current tree root.
+    ///
+    /// - Returns:
+    ///     - Validation: if subtree rooted from current `root` is valid.
+    ///     - Minimum: min value of tree rooted from `root`.
+    ///     - Maximum: max value of tree rooted from `root`
+    ///     - Sum: the sum of every TreeNode child, including `root`
+    ///
+    /// - Complexity: O(n), n is the number of nodes in this tree rooted from `root`
+    ///
     private func validBST(_ root: TreeNode) -> (Bool, Int, Int, Int) {
         var isValid = true
         var minV = root.val
@@ -42,6 +60,7 @@ class Solution {
         let leftSum: Int
         let rightSum: Int
         
+        /// If `root` has left child.
         if let left = root.left {
             let (lvalid, lmin, lmax, lsum) = self.validBST(left)
             if lmax >= minV { isValid = false }
@@ -51,6 +70,8 @@ class Solution {
         } else {
             leftSum = 0
         }
+        
+        /// If `root` has right child.
         if let right = root.right {
             let (rvalid, rmin, rmax, rsum) = self.validBST(right)
             if rmin <= maxV { isValid = false }
@@ -60,9 +81,15 @@ class Solution {
         } else {
             rightSum = 0
         }
+        
+        /// If current tree rooted from `root` is a valid BST.
         if isValid {
-            maxSum = max(maxSum, sum + leftSum + rightSum)
+            sum += leftSum + rightSum
+            maxSum = max(maxSum, sum)
+        } else {
+            sum = 0
         }
-        return (isValid, minV, maxV, isValid ? sum + leftSum + rightSum : 0)
+        
+        return (isValid, minV, maxV, sum)
     }
 }
