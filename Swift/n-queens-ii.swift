0ff8c48af125c9713af1b562132acf2e426914dc
Antonio081014 <antonio081014@antonio081014.com>
2020-03-24 11:53:19 -0700

    Rewrite n-queens-ii

--- Swift/n-queens-ii.swift
+++ Swift/n-queens-ii.swift
@@ -58,3 +58,58 @@ class Solution {
         return solution.map({"\($0)"}).joined(separator: "-")
     }
 }
+
+// Tue Mar 24 11:53:03 PDT 2020
+class Solution {
+    func totalNQueens(_ n: Int) -> Int {
+        var result: Int = 0
+        var solution: [Int] = []
+        self.selectNthQueen(0, n, &solution, &result)
+        return result
+    }
+
+    /// Try to select nth queen's column position at the nth row.
+    /// - Parameters: 
+    ///     - nth: nth row, the row will be assigned a queen at one of available column position.
+    ///     - totalQueens: the total number of queens will be available on the board.
+    ///     - solution: existing selected columns positions. solution[index] is the column position for index-th queen at row index.
+    ///     - result: final visualized graph solution for this problem.
+    ///
+    fileprivate func selectNthQueen(_ nth: Int, _ totalQueens: Int, _ solution: inout [Int], _ result: inout Int) {
+        // Add the valid solution by 1.
+        func convert2Result() {
+            result += 1
+        }
+
+        // Validate if it's a legal col, criteria
+        // 1. Not the same col with any existing col in current solution, since no two queens could share the same column.
+        // 2. Not able to attack diagonally.
+        func validation(_ col: Int) -> Bool {
+            for index in 0 ..< solution.count {
+                if col == solution[index] { return false }
+                if abs(solution.count - index) == abs(col - solution[index]) { return false }
+            }
+            return true
+        }
+
+        // Start with base case.
+        if nth == totalQueens {
+            convert2Result()
+            return
+        }
+
+        // Go through every possible column.
+        // Validate if it's a legal col.
+        // If it is, then add it to solution temporarily.
+        // Then DFS this solution.
+        // When done, we should try next solution. 
+        // But before we try the next one, remove current selection for nth row is necessery.
+        for x in 0 ..< totalQueens {
+            if validation(x) {
+                solution.append(x)
+                self.selectNthQueen(nth + 1, totalQueens, &solution, &result)
+                solution.removeLast()
+            }
+        }
+    }
+}
