f276cb9b565a8ff71b0ca22f177cee207b73831a
Antonio081014 <antonio081014@antonio081014.com>
2020-03-25 14:17:44 -0700

    Rewrite add-two-numbers.swift

--- Swift/add-two-numbers.swift
+++ Swift/add-two-numbers.swift
@@ -37,3 +37,27 @@ class Solution {
         return head.next
     }
 }
+
+// Wed Mar 25 14:16:12 PDT 2020
+class Solution {
+    ///
+    /// - Complexity: O(max(L1, L2)), where L1 is the length of l1, L2 is the length of l2.
+    ///
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        let root = ListNode(0)
+        var parent: ListNode? = root
+        var ll1 = l1
+        var ll2 = l2
+        var sum = 0
+        while ll1 != nil || ll2 != nil || sum > 0 {
+            sum += ll1?.val ?? 0
+            sum += ll2?.val ?? 0
+            ll1 = ll1?.next
+            ll2 = ll2?.next
+            parent?.next = ListNode(sum % 10)
+            parent = parent?.next
+            sum /= 10
+        }
+        return root.next
+    }
+}
