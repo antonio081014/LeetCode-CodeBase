6bfc21fe02cdd8cea26ac30e6f2a3b33aa58832d
Antonio081014 <antonio081014@antonio081014.com>
2020-03-25 14:06:38 -0700

    Rewrite two-sum.swift

--- Swift/two-sum.swift
+++ Swift/two-sum.swift
@@ -15,3 +15,25 @@ class Solution {
         return [Int]()
     }
 }
+
+// Wed Mar 25 14:03:04 PDT 2020
+class Solution {
+    /// Go through every element in the array.
+    /// While remembering the index of every element visited.
+    /// If target - nums[index] is in the indexMap, which means that element has been visited before.
+    /// then, return the indices of these two.
+    /// otherwise, keep visiting every element, while remember it and its index.
+    ///
+    /// - Complexity: O(n), n is the number of element in the array.
+    ///
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var indexMap: [Int : Int] = [:]
+        for index in 0 ..< nums.count {
+            if let pIndex = indexMap[target - nums[index]] {
+                return [pIndex, index]
+            }
+            indexMap[nums[index]] = index
+        }
+        return [0, 0]
+    }
+}
