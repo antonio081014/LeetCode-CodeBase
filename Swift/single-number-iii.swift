75feb91c03fd7e696a9e6f867b8648fb1ad7a469
Antonio081014 <antonio081014@antonio081014.com>
2020-04-01 15:05:14 -0700

    Rewrite single-number-iii.swift

--- Swift/single-number-iii.swift
+++ Swift/single-number-iii.swift
@@ -16,3 +16,40 @@ class Solution {
         return Array(list)
     }
 }
+
+
+class Solution {
+    func singleNumber(_ nums: [Int]) -> [Int] {
+        var diff = 0
+        for n in nums {
+            diff ^= n
+        }
+        /// This will help get the rightmost bit set, start with 1st '1' bit from right.
+        /// Example:
+        ///     7 and 1
+        ///     diff = XOR(7, 1) = 0b0110,
+        ///     diff ^= diff, diff = 0b0010,
+        ///         So the second bit from right will be the key ones to seperate a and b.
+        ///
+        ///     7 and 2
+        ///     diff = XOR(7, 2) = 0b0101,
+        ///     diff ^= diff, diff = 0b0001,
+        ///         So the first bit from right will be the key ones to seperate a and b.
+        ///
+        /// The key is to find the one key bit to diff one group from another.
+        ///
+        diff &= -diff
+        
+        var a = 0
+        var b = 0
+        
+        for n in nums {
+            if n & diff == 0 {
+                a ^= n
+            } else {
+                b ^= n
+            }
+        }
+        return [a, b]
+    }
+}
