f4a6f868f30d1d5c9be7aca7768ba3a147865466
Antonio081014 <antonio081014@antonio081014.com>
2020-04-01 10:13:21 -0700

    Add single-number-iii.swift

--- Swift/single-number-iii.swift
+++ Swift/single-number-iii.swift
@@ -50,3 +50,21 @@ func singleNumber2(_ nums: [Int]) -> [Int] {
         return dict
     }).flatMap{key, value in (value % 2 == 0 ? nil : key)}
 }
+/**
+ * https://leetcode.com/problems/single-number-iii/
+ * 
+ * 
+ */ 
+class Solution {
+    func singleNumber(_ nums: [Int]) -> [Int] {
+        var list: Set<Int> = []
+        for n in nums {
+            if list.contains(n) {
+                list.remove(n)
+            } else {
+                list.insert(n)
+            }
+        }
+        return Array(list)
+    }
+}
