b7a5026e3506cf8e9d94c89024e566a50bed91de
Antonio081014 <antonio081014@antonio081014.com>
2020-04-01 10:37:09 -0700

    Add a more general solution to single-number-ii.swift

--- Swift/single-number-ii.swift
+++ Swift/single-number-ii.swift
@@ -6,6 +6,23 @@
 
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
+        return self.singleNumber(nums, with: 3)
+    }
+    
+    fileprivate func singleNumber(_ nums: [Int], with repeatNumber: Int) -> Int {
+        var ret = 0
+        for bit in 0 ..< 64 {
+            var count = 0
+            for n in nums {
+                if ((n >> bit) & 1) == 1 { count += 1 }
+            }
+            count %= repeatNumber
+            ret |= (count << bit)
+        }
+        return ret
+    }
+    
+    func singleNumber(_ nums: [Int]) -> Int {
         var count: [Int : Int] = [:]
         for n in nums {
             count[n] = (count[n, default: 0] + 1)
