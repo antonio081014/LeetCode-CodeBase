ac7c6ceab019136ca2e29dbfc823201610a5cb10
Antonio081014 <antonio081014@antonio081014.com>
2020-04-10 13:33:07 -0700

    Add power-of-four.swift

--- Swift/power-of-four.swift
+++ Swift/power-of-four.swift
@@ -20,3 +20,25 @@ class Solution {
         return ((num & (num-1)) == 0) && ((num-1) % 3 == 0)
     }
 }
+/**
+ * https://leetcode.com/problems/power-of-four/
+ * 
+ * 
+ */ 
+class Solution {
+    func isPowerOfFour(_ num: Int) -> Bool {
+        var n = num
+        while n > 1 {
+            if n % 4 != 0 { return false }
+            n /= 4
+        }
+        return n == 1
+    }
+}
+
+class Solution {
+    func isPowerOfFour(_ num: Int) -> Bool {
+        let mask = 0b01010101010101010101010101010101
+        return num>0 && (num&(num - 1)) == 0 && (num | mask) == mask
+    }
+}
