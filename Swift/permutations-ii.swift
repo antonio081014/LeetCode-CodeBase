c9002e337e1c6c8944e3bb3faac0255e41e44abe
Antonio081014 <antonio081014@antonio081014.com>
2020-04-10 22:53:18 -0700

    Add permutations-ii.swift

--- Swift/permutations-ii.swift
+++ Swift/permutations-ii.swift
@@ -24,3 +24,36 @@ class Solution {
         }
     }
 }
+/**
+ * https://leetcode.com/problems/permutations-ii/
+ * 
+ * 
+ */ 
+class Solution {
+    func permuteUnique(_ nums: [Int]) -> [[Int]] {
+        var cand: [Int : Int] = [:]
+        for n in nums {
+            cand[n] = 1 + cand[n, default: 0]
+        }
+        var ret: [[Int]] = []
+        process(&ret, [], &cand)
+        return ret
+    }
+    
+    /// Use a map to remove duplicate cases.
+    /// In this method, e.g, when we use a 1, we don't care which 1 we are using, we only cares about how many
+    /// left.
+    ///
+    fileprivate func process(_ result: inout [[Int]], _ solution: [Int], _ candidate: inout [Int : Int]) {
+        if candidate.isEmpty {
+            result.append(solution)
+            return
+        }
+        
+        for (key, value) in candidate {
+            candidate[key] = value == 1 ? nil : value - 1
+            process(&result, solution + [key], &candidate)
+            candidate[key] = value
+        }
+    }
+}
