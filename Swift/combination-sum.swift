45a3768bc073ce8556d1548ffb6fc1eb2a856ed4
Antonio081014 <antonio081014@antonio081014.com>
2020-04-10 17:33:51 -0700

    Add faster solution to combination-sum.swift

--- Swift/combination-sum.swift
+++ Swift/combination-sum.swift
@@ -26,3 +26,33 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var result = [[Int]]()
+        var current = [Int]()
+        let cand = candidates.sorted()
+        if target >= cand[0] {
+            for n in 1 ... Int(target / cand[0]) {
+                dfs(cand, target, 0, 0, n, &current, &result)
+            }
+        }
+        return result
+    }
+    
+    fileprivate func dfs(_ candidates: [Int], _ target: Int, _ start: Int, _ deepth: Int, _ maxDeepth: Int, _ current: inout [Int], _ result: inout [[Int]]) {
+        if deepth == maxDeepth {
+            if target == 0 {
+                result.append(current)
+            }
+            return
+        }
+        
+        for index in start ..< candidates.count {
+            if candidates[index] > target { break }
+            current.append(candidates[index])
+            dfs(candidates, target - candidates[index], index, deepth + 1, maxDeepth, &current, &result)
+            current.removeLast()
+        }
+    }
+}
