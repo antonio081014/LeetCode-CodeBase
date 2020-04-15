f16aae101130231f151e01ccaffd086b3d8f7aae
Antonio081014 <antonio081014@antonio081014.com>
2020-04-15 10:13:50 -0700

    Add product-of-array-except-self.swift

--- Swift/product-of-array-except-self.swift
+++ Swift/product-of-array-except-self.swift
@@ -18,3 +18,42 @@ class Solution {
         return prod
     }
 }
+/**
+ * https://leetcode.com/problems/product-of-array-except-self/
+ * 
+ * 
+ */ 
+class Solution {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var result: [Int] = Array(repeating: 1, count: nums.count)
+        
+        var prod = 1
+        for index in 0 ..< nums.count {
+            result[index] *= prod
+            prod *= nums[index]
+        }
+        prod = 1
+        for index in (0 ..< nums.count).reversed() {
+            result[index] *= prod
+            prod *= nums[index]
+        }
+        return result
+    }
+    
+    func productExceptSelf2(_ nums: [Int]) -> [Int] {
+        var leftp = [1]
+        var rightp = [1]
+        for index in 1 ..< nums.count {
+            let left = leftp.last ?? 1
+            leftp.append(left * nums[index - 1])
+            let right = rightp.first ?? 1
+            rightp.insert(right * nums[nums.count - index], at: 0)
+        }
+        
+        var result: [Int] = []
+        for index in 0 ..< nums.count {
+            result.append(leftp[index] * rightp[index])
+        }
+        return result
+    }
+}
