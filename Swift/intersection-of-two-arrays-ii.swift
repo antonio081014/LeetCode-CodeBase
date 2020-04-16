351e1879218a4ddcff605abd2359bf07c2bf5f92
Antonio081014 <antonio081014@antonio081014.com>
2020-04-15 22:57:22 -0700

    Add comment to intersection-of-two-arrays-ii

--- Swift/intersection-of-two-arrays-ii.swift
+++ Swift/intersection-of-two-arrays-ii.swift
@@ -23,3 +23,14 @@ class Solution {
         return ret
     }
 }
+///
+/// Follow up:
+/// - What if the given array is already sorted? How would you optimize your algorithm?
+///     We could use two-pointer algorithm to go through both arrays. It will take O(max(m, n), where m and n are the lenght of these two arrays.
+/// - What if nums1's size is small compared to nums2's size? Which algorithm is better?
+///     For two sorted array, we could use binary search in the bigger array to find each element in the smaller array. It will cost O(mlogn), where n is far larger than m.
+/// - What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+///     1. If one of these arrays are not that big, could fit in the memory, then we retrieve certain elements of the other array from disk, and compare.
+///     2. If both of these two are really large, we could have them sorted, like merge-sort. Then, use two-pointers algorithm, get elements of two arrays from disk, then compare. Keep doing this.
+///
+
