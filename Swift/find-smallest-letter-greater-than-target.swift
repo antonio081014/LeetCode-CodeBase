2f743c5c5894eb6e8ab6a11a8f3d1e8b7e3b0b70
Antonio081014 <antonio081014@antonio081014.com>
2020-04-15 21:58:02 -0700

    Add comment to find-smallest-letter-greater-than-target.swift

--- Swift/find-smallest-letter-greater-than-target.swift
+++ Swift/find-smallest-letter-greater-than-target.swift
@@ -4,6 +4,12 @@
  * 
  */ 
 class Solution {
+    ///
+    /// The key part of this binary search is:
+    /// 1. left will be the first possible valid choice.
+    /// 2. right - left >= 1, then stop when left == right
+    /// 3. In the case left move to all the way to the end, but right never changed, we didn't find the valid choice.
+    ///
     func nextGreatestLetter(_ letters: [Character], _ target: Character) -> Character {
         var left = 0
         var right = letters.count
