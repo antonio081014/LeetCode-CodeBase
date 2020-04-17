d98534925c0cafd669a2d380f703254396d2415b
Antonio081014 <antonio081014@antonio081014.com>
2020-04-17 09:12:37 -0700

    Add bfs and dfs solutions to number-of-islands.swift

--- Swift/number-of-islands.swift
+++ Swift/number-of-islands.swift
@@ -2,7 +2,12 @@
  * https://leetcode.com/problems/number-of-islands/
  * 
  * 
- */ 
+ */
+/// DFS
+/// - Complexity:
+///     - Time: O(n), n is the number elements in grid. n = rows * cols
+///     - Space: O(n), n is the number elements in grid. n = rows * cols
+///
 class Solution {
     func numIslands(_ grid: [[Character]]) -> Int {
         guard grid.count > 0 else { return 0 }
@@ -30,5 +35,43 @@ class Solution {
                 dfs(&grid, (xx, yy))
             }
         }
+    }
+}
+
+/// BFS
+/// - Complexity:
+///     - Time: O(n), n is the number elements in grid. n = rows * cols
+///     - Space: O(n), n is the number elements in grid. n = rows * cols
+///
+class Solution {
+    func numIslands(_ grid: [[Character]]) -> Int {
+        guard grid.count > 0 else { return 0 }
+        var grid = grid
+        var count = 0
+        for x in 0 ..< grid.count {
+            for y in 0 ..< grid.first!.count {
+                if grid[x][y] == Character("1") {
+                    count += 1
+                    grid[x][y] = Character("0")
+                    
+                    var queue = [(x, y)]
+                    while queue.isEmpty == false {
+                        let (xx, yy) = queue.removeFirst()
+                        let dx = [0, 1, 0, -1]
+                        let dy = [1, 0, -1, 0]
+                        for index in 0 ..< dx.count {
+                            let xxx = xx + dx[index]
+                            let yyy = yy + dy[index]
+                            if xxx >= 0, xxx < grid.count, yyy >= 0, yyy < grid.first!.count, grid[xxx][yyy] == Character("1") {
+                                grid[xxx][yyy] = Character("0")
+                                queue.append((xxx, yyy))
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        }
+        return count
     }
 }
