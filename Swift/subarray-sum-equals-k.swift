3a1e4947fa371995bd5c7686cf27d57b8c7879d3
Antonio081014 <antonio081014@antonio081014.com>
2020-04-22 11:49:57 -0700

    Add subarray-sum-equals-k.swift

--- Swift/subarray-sum-equals-k.swift
+++ Swift/subarray-sum-equals-k.swift
@@ -24,3 +24,27 @@ class Solution {
         return count
     }
 }
+/**
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ * 
+ * 
+ */ 
+// Date: Wed Apr 22 11:49:46 PDT 2020
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(n), n is the number of elements in the array.
+    ///     - Space: O(n)
+    func subarraySum(_ nums: [Int], _ k: Int) -> Int {
+        var count = 0
+        var sumCount: [Int : Int] = [0 : 1]
+        var sum = 0
+        for n in nums {
+            sum += n
+            if let sumC = sumCount[sum - k] {
+                count += sumC
+            }
+            sumCount[sum] = 1 + sumCount[sum, default: 0]
+        }
+        return count
+    }
+}
