050cf5f926f09a349252dc5ff90d5ae7f558d6d2
Antonio081014 <antonio081014@antonio081014.com>
2020-04-26 10:30:50 -0700

    Add longest-consecutive-sequence.swift

--- Swift/longest-consecutive-sequence.swift
+++ Swift/longest-consecutive-sequence.swift
@@ -28,3 +28,36 @@ class Solution {
         return maxn
     }
 }
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * 
+ * 
+ */ 
+// Date: Sun Apr 26 10:30:40 PDT 2020
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(n)
+    ///     - Space: O(n)
+    ///     Although the time complexity appears to be quadratic due to the while loop nested within the for loop, closer inspection reveals it to be linear. Because the while loop is reached only when currentNum marks the beginning of a sequence (i.e. currentNum-1 is not present in nums), the while loop can only run for n iterations throughout the entire runtime of the algorithm. This means that despite looking like O( n ⋅ n ) complexity, the nested loops actually run in O(n+n)=O(n) time. All other computations occur in constant time, so the overall runtime is linear.
+    ///
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        var set: Set<Int> = []
+        for n in nums {
+            set.insert(n)
+        }
+        
+        var longestCS = 0
+        for n in set {
+            if set.contains(n - 1) == false {
+                var count = 1
+                var current = n
+                while set.contains(current + 1) {
+                    current += 1
+                    count += 1
+                }
+                longestCS = max(longestCS, count)
+            }
+        }
+        return longestCS
+    }
+}
