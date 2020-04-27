6f4656fc814d63874a6067799fc3e1d052f571b3
Antonio081014 <antonio081014@antonio081014.com>
2020-04-27 09:08:44 -0700

    Add maximal-square.swift

--- Swift/maximal-square.swift
+++ Swift/maximal-square.swift
@@ -34,3 +34,39 @@ class Solution {
         return maxn * maxn
     }
 }
+/**
+ * https://leetcode.com/problems/maximal-square/
+ * 
+ * 
+ */ 
+// Date: Mon Apr 27 09:08:34 PDT 2020
+class Solution {
+    ///
+    /// `square[x][y]` indicates the largest size of square whose right bottom is at (x, y).
+    /// 
+    /// - Complexity:
+    ///     - Time: O(m*n), m and n are the width and height of matrix.
+    ///     - Space: O(m*n), m and n are the width and height of matrix.
+    ///
+    func maximalSquare(_ matrix: [[Character]]) -> Int {
+        let n = matrix.count
+        guard let m = matrix.first?.count else { return 0 }
+        var square = Array(repeating: Array(repeating: 0, count: m), count: n)
+        
+        var maxCount = 0
+        for x in 0 ..< n {
+            for y in 0 ..< m {
+                if x == 0 || y == 0 {
+                    square[x][y] = matrix[x][y] == Character("1") ? 1 : 0
+                } else {
+                    if matrix[x][y] == Character("1") {
+                        square[x][y] = 1 + min(square[x - 1][y - 1], min(square[x - 1][y], square[x][y - 1]))
+                    }
+                }
+                maxCount = max(maxCount, square[x][y])
+            }
+        }
+        // print("\(square)")
+        return maxCount * maxCount
+    }
+}
