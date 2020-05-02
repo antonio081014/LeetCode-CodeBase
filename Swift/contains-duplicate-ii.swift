50fbd110fc3d1dbf53242952f00897ef70884460
Antonio081014 <antonio081014@antonio081014.com>
2020-05-02 12:52:26 -0700

    Add contains-duplicate-ii.swift

--- Swift/contains-duplicate-ii.swift
+++ Swift/contains-duplicate-ii.swift
@@ -19,3 +19,21 @@ class Solution {
         return false
     }
 }
+/**
+ * https://leetcode.com/problems/contains-duplicate-ii/
+ * 
+ * 
+ */ 
+// Date: Sat May  2 12:52:15 PDT 2020
+class Solution {
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var lastIndex: [Int : Int] = [:]
+        for index in 0 ..< nums.count {
+            if let last = lastIndex[nums[index]], index - last <= k {
+                return true
+            }
+            lastIndex[nums[index]] = index
+        }
+        return false
+    }
+}
