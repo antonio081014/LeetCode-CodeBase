fb78a4b64bbb74eda431d2448b92271fba5fa367
Antonio081014 <antonio081014@antonio081014.com>
2020-05-02 13:00:44 -0700

    Add comment to jewels-and-stones.swift

--- Swift/jewels-and-stones.swift
+++ Swift/jewels-and-stones.swift
@@ -5,6 +5,10 @@
  */ 
 // Date: Sat May  2 12:59:05 PDT 2020
 class Solution {
+    /// - Complexity:
+    ///     - Time: O(len(J) + len(S))
+    ///     - Space: O(len(J))
+    ///
     func numJewelsInStones(_ J: String, _ S: String) -> Int {
         let jews = Set<String>(Array(J).map { String($0) })
         var count = 0
