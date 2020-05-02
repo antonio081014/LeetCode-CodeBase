41f9a6157685d7be6c8cd4b229ff9462ed258185
Antonio081014 <antonio081014@antonio081014.com>
2020-05-01 23:16:31 -0700

    Add remove-duplicates-from-sorted-array-ii.swift

--- Swift/remove-duplicates-from-sorted-array-ii.swift
+++ Swift/remove-duplicates-from-sorted-array-ii.swift
@@ -23,4 +23,30 @@ class Solution {
         }
         return nums.count
     }
+}/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+ * 
+ * 
+ */ 
+// Date: Fri May  1 23:16:18 PDT 2020
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        if nums.count < 2 { return nums.count }
+        var startIndex = 1
+        var dupCount = 1
+        for index in 1 ..< nums.count {
+            if nums[index] == nums[index - 1] {
+                dupCount += 1
+                if dupCount <= 2 {
+                    nums[startIndex] = nums[index]
+                    startIndex += 1
+                }
+            } else {
+                nums[startIndex] = nums[index]
+                startIndex += 1
+                dupCount = 1
+            }
+        }
+        return startIndex
+    }
 }
