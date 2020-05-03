ec6d57909c4117a1fd11373486719437a9360d38
Antonio081014 <antonio081014@antonio081014.com>
2020-05-03 15:33:03 -0700

    Add first-unique-character-in-a-string.swift

--- Swift/first-unique-character-in-a-string.swift
+++ Swift/first-unique-character-in-a-string.swift
@@ -23,4 +23,25 @@ class Solution {
         }
         return minP == s.characters.count ? -1 : minP
     }
+}/**
+ * https://leetcode.com/problems/first-unique-character-in-a-string/
+ * 
+ * 
+ */ 
+// Date: Sun May  3 15:32:53 PDT 2020
+class Solution {
+    func firstUniqChar(_ s: String) -> Int {
+        var count: [String: Int] = [:]
+        let s = Array(s)
+        for c in s {
+            count[String(c)] = 1 + count[String(c), default: 0]
+        }
+        
+        for index in 0 ..< s.count {
+            if 1 == count[String(s[index])]! {
+                return index
+            }
+        }
+        return -1
+    }
 }
