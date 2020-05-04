03a7dfef7829408acc37a637e421a4e327a62c06
Antonio081014 <antonio081014@antonio081014.com>
2020-05-04 10:06:49 -0700

    Add rotate-array.swift

--- Swift/rotate-array.swift
+++ Swift/rotate-array.swift
@@ -20,3 +20,27 @@ class Solution {
     
     /// Other solution might use reverse array to solve this problem.
 }
+/**
+ * https://leetcode.com/problems/rotate-array/
+ * 
+ * 
+ */ 
+// Date: Mon May  4 10:06:39 PDT 2020
+class Solution {
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        let k = k % nums.count
+        reverse(&nums, 0, nums.count - 1)
+        reverse(&nums, 0, k - 1)
+        reverse(&nums, k, nums.count - 1)
+    }
+    
+    private func reverse(_ nums: inout [Int], _ start: Int, _ end: Int) {
+        var start = start
+        var end = end
+        while start < end {
+            nums.swapAt(start, end)
+            start += 1
+            end -= 1
+        }
+    }
+}
