11e58167fd622c3afda13f55ed813fa377abd662
Antonio081014 <antonio081014@antonio081014.com>
2020-05-05 14:24:05 -0700

    Add maximum-depth-of-binary-tree.swift

--- Swift/maximum-depth-of-binary-tree.swift
+++ Swift/maximum-depth-of-binary-tree.swift
@@ -25,3 +25,32 @@ class Solution {
         return 1 + max(maxDepth(root.left), maxDepth(root.right))
     }
 }
+/**
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ * 
+ * 
+ */ 
+// Date: Tue May  5 14:23:54 PDT 2020
+class Solution {
+    /// Non-recursive method by using queue level traverse a tree.
+    ///  - Complexity:
+    ///     - Time: O(n), n is the number of nodes in the tree
+    ///     - Space: O(n/2), n/2 is the number of leaf nodes for a full binary tree.
+    ///
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        var queue = [root]
+        var depth = 0
+        while !queue.isEmpty {
+            depth += 1
+            var n = queue.count
+            while n > 0 {
+                n -= 1
+                let node = queue.removeFirst()
+                if let left = node.left { queue.append(left) }
+                if let right = node.right { queue.append(right) }
+            }
+        }
+        return depth
+    }
+}
