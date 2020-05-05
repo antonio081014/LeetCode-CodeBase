595bc0b69d0a0cf56bee3f2292be1d62fa6c1591
Antonio081014 <antonio081014@antonio081014.com>
2020-05-05 14:31:42 -0700

    Add maximum-depth-of-n-ary-tree.swift

--- Swift/maximum-depth-of-n-ary-tree.swift
+++ Swift/maximum-depth-of-n-ary-tree.swift
@@ -35,3 +35,31 @@ class Solution {
         return depth
     }
 }
+/**
+ * https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+ * 
+ * 
+ */ 
+// Date: Tue May  5 14:31:31 PDT 2020
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var children: [Node]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.children = []
+ *     }
+ * }
+ */
+
+class Solution {
+    func maxDepth(_ root: Node?) -> Int {
+        guard let root = root else { return 0 }
+        var maxDep = 0
+        for child in root.children {
+            maxDep = max(maxDep, maxDepth(child))
+        }
+        return 1 + maxDep
+    }
+}
