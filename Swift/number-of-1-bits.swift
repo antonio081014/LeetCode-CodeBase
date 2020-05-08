02da46973a06da18708abe6288edc4fac8401c88
Antonio081014 <antonio081014@antonio081014.com>
2020-05-08 13:53:17 -0700

    Add number-of-1-bits.swift

--- Swift/number-of-1-bits.swift
+++ Swift/number-of-1-bits.swift
@@ -15,3 +15,23 @@ class Solution {
         return count
     }
 }
+/**
+ * https://leetcode.com/problems/number-of-1-bits/
+ * 
+ * 
+ */ 
+// Date: Fri May  8 13:53:06 PDT 2020
+class Solution {
+    ///
+    ///n the binary representation, the least significant 1-bit in n always corresponds to a 0-bit in  n−1. Therefore, anding the two numbers n and n−1 always flips the least significant 1-bit in n to 0, and keeps all other bits the same.
+    ///
+    func hammingWeight(_ n: Int) -> Int {
+        var n = n
+        var count = 0
+        while n > 0 {
+            count += 1
+            n &= (n - 1)
+        }
+        return count
+    }
+}
