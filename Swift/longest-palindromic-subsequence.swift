113f88e71ea00b099438eb1d3cf9d6c120256525
Antonio081014 <antonio081014@antonio081014.com>
2020-05-14 11:31:26 -0700

    Add longest-palindromic-subsequence.swift

--- Swift/longest-palindromic-subsequence.swift
+++ Swift/longest-palindromic-subsequence.swift
@@ -31,3 +31,31 @@ class Solution {
         return isPalindromic[0][s.count - 1]
     }
 }
+/**
+ * https://leetcode.com/problems/longest-palindromic-subsequence/
+ * 
+ * 
+ */ 
+// Date: Thu May 14 11:31:15 PDT 2020
+class Solution {
+    /// Faster with less comparison function `max` used.
+    func longestPalindromeSubseq(_ s: String) -> Int {
+        let s = Array(s)
+        var isPalindromic: [[Int]] = Array(repeating: Array(repeating: 0, count: s.count), count: s.count)
+        for len in 1 ... s.count {
+            for start in 0 ... (s.count - len) {
+                let end = start + len - 1
+                if len == 1 {
+                    isPalindromic[start][end] = 1
+                } else {
+                    if s[start] == s[end] {
+                        isPalindromic[start][end] = max(isPalindromic[start][end], isPalindromic[start + 1][end - 1] + 2)
+                    } else {
+                        isPalindromic[start][end] = max(isPalindromic[start][end - 1], isPalindromic[start + 1][end])
+                    }
+                }
+            }
+        }
+        return isPalindromic[0][s.count - 1]
+    }
+}
