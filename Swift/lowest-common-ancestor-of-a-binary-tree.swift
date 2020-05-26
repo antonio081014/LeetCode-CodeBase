4979360f2b8e77ba083b2b961962d825aee4648d
Antonio081014 <antonio081014@antonio081014.com>
2020-05-26 11:30:57 -0700

    Add lowest-common-ancestor-of-a-binary-tree.swift

--- Swift/lowest-common-ancestor-of-a-binary-tree.swift
+++ Swift/lowest-common-ancestor-of-a-binary-tree.swift
@@ -37,3 +37,73 @@ class Solution {
         return nil
     }
 }
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ * 
+ * 
+ */ 
+// Date: Tue May 26 11:30:46 PDT 2020
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+/// Extension to make TreeNode hashable, so it could be used as a key in dictionary.
+extension TreeNode: Hashable {
+    public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return lhs.val == rhs.val
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.val)
+    }
+}
+
+class Solution {
+    /// Let var parent: [TreeNode : TreeNode?] keep all the parent nodes of each node in the tree.
+    /// Then, add all the nodes from p to root to the Set
+    /// Then, traverse from q to root, to find the first common ancester along the path.
+    /// - Complexity:
+    ///     - Time: O(n), n is the number of nodes in the tree.
+    ///     - Space: O(n), n is the number of nodes in the tree. queue uses O(height of tree), dictionary uses O(n). list uses O(height of tree)
+    ///
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        guard let root = root, let p = p, let q = q else { return nil }
+        var parent: [TreeNode : TreeNode?] = [root : nil]
+        var queue = [root]
+        
+        while parent[p] == nil || parent[q] == nil {
+            let node = queue.removeLast()
+            if let left = node.left {
+                parent[left] = node
+                queue.append(left)
+            }
+            if let right = node.right {
+                parent[right] = node
+                queue.append(right)
+            }
+        }
+        
+        var list: Set<TreeNode> = []
+        var node: TreeNode? = p
+        while node != nil {
+            list.insert(node!)
+            node = parent[node!]!
+        }
+        
+        node = q
+        while list.contains(node!) == false {
+            node = parent[node!]!
+        }
+        return node
+    }
+}
