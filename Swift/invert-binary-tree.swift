0a4a0bbc39f9be2e32e14ae39b8668d9f64fb4d1
Antonio081014 <antonio081014@antonio081014.com>
2020-06-01 10:30:37 -0700

    Add invert-binary-tree.swift

--- Swift/invert-binary-tree.swift
+++ Swift/invert-binary-tree.swift
@@ -88,3 +88,43 @@ class Solution {
         return root
     }
 }
+/**
+ * https://leetcode.com/problems/invert-binary-tree/
+ * 
+ * 
+ */ 
+// Date: Mon Jun  1 10:30:27 PDT 2020
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let root = root else { return nil }
+        var queue = [root]
+        while queue.isEmpty == false {
+            let node = queue.removeFirst()
+            let tmpNode = node.left
+            node.left = node.right
+            node.right = tmpNode
+            if let left = node.left {
+                queue.append(left)
+            }
+            if let right = node.right {
+                queue.append(right)
+            }
+        }
+        return root
+    }
+}
