c4bde1d29f985606274b05a282d9b9b4b68fd4df
Antonio081014 <antonio081014@antonio081014.com>
2020-06-01 14:26:22 -0700

    Add best-time-to-buy-and-sell-stock-with-cooldown.swift

--- Swift/best-time-to-buy-and-sell-stock-with-cooldown.swift
+++ Swift/best-time-to-buy-and-sell-stock-with-cooldown.swift
@@ -19,3 +19,23 @@ class Solution {
         return dp[n]
     }
 }
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ * 
+ * 
+ */ 
+// Date: Mon Jun  1 14:26:11 PDT 2020
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        let n = prices.count
+        guard n > 1 else { return 0 }
+        var dp = Array(repeating: 0, count: n + 1)
+        var minCost = Int.max
+        for sell in 2 ... n {
+            minCost = min(minCost, prices[sell - 2] - (sell > 2 ? dp[sell - 3] : 0))
+            dp[sell] = max(dp[sell - 1], prices[sell - 1] - minCost)
+        }
+        // print("\(dp)")
+        return dp[n]
+    }
+}
