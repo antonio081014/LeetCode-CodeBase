1857e14f28e4ca769842b6b87e2bef445d577cd5
Antonio081014 <antonio081014@antonio081014.com>
2020-07-01 09:35:08 -0700

    Add arranging-coins.swift

--- Swift/arranging-coins.swift
+++ Swift/arranging-coins.swift
@@ -19,3 +19,28 @@ class Solution {
         return left - 1
     }
 }
+/**
+ * https://leetcode.com/problems/arranging-coins/
+ * 
+ * 
+ */ 
+// Date: Wed Jul  1 09:34:58 PDT 2020
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(logn), using a binary search, nothing else could be expected here.
+    ///     - Space: O(1), since we only have left, right.
+    ///
+    func arrangeCoins(_ n: Int) -> Int {
+        var left = 1
+        var right = n + 1
+        while left < right {
+            let mid = left + (right - left) / 2
+            if mid * (mid + 1) <= n * 2 {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+        return left - 1
+    }
+}
