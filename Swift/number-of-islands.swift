f86b80c9c5eb0186091514c98b5edb6a9044610f
Antonio081014 <antonio081014@antonio081014.com>
2020-07-07 11:53:59 -0700

    Add number-of-islands.swift

--- Swift/number-of-islands.swift
+++ Swift/number-of-islands.swift
@@ -75,3 +75,40 @@ class Solution {
         return count
     }
 }
+/**
+ * https://leetcode.com/problems/number-of-islands/
+ * 
+ * 
+ */ 
+// Date: Tue Jul  7 11:53:48 PDT 2020
+class Solution {
+    func numIslands(_ grid: [[Character]]) -> Int {
+        var grid = grid
+        let n = grid.count
+        guard let m = grid.first?.count else { return 0 }
+        var count = 0
+        for x in 0 ..< n {
+            for y in 0 ..< m {
+                if grid[x][y] == Character("1") {
+                    count += 1
+                    var queue = [(x, y)]
+                    grid[x][y] = Character("0")
+                    let dx = [1, 0, -1, 0]
+                    let dy = [0, 1, 0, -1]
+                    while queue.isEmpty == false {
+                        let (xx, yy) = queue.removeFirst()
+                        for index in 0 ..< dx.count {
+                            let xxx = xx + dx[index]
+                            let yyy = yy + dy[index]
+                            if xxx >= 0, xxx < n, yyy >= 0, yyy < m, grid[xxx][yyy] == Character("1") {
+                                grid[xxx][yyy] = Character("0")
+                                queue.append((xxx, yyy))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count
+    }
+}
