2963503d797493b03573e4b25f0be9a7845c9a01
Antonio081014 <antonio081014@antonio081014.com>
2020-07-09 11:59:54 -0700

    Add sqrtx.swift

--- Swift/sqrtx.swift
+++ Swift/sqrtx.swift
@@ -18,4 +18,40 @@ class Solution {
         }
         return left - 1
     }
+}/**
+ * https://leetcode.com/problems/sqrtx/
+ * 
+ * 
+ */ 
+// Date: Thu Jul  9 11:59:43 PDT 2020
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        var left = 1
+        var right = x + 1
+        while left < right {
+            let mid = left + (right - left) / 2
+            if mid * mid >= x {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return left * left == x ? left : left - 1
+    }
+}
+
+class Solution {
+    func mySqrt(_ x: Int) -> Int {
+        var left = 1
+        var right = x + 1
+        while left < right {
+            let mid = left + (right - left) / 2
+            if mid * mid > x {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return left - 1
+    }
 }
