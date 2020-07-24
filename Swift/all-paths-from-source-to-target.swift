9faf5e8d6b00118e937b3f5b7b9c714f55e2f978
Antonio081014 <antonio081014@antonio081014.com>
2020-07-24 09:44:48 -0700

    Add all-paths-from-source-to-target.swift

--- Swift/all-paths-from-source-to-target.swift
+++ Swift/all-paths-from-source-to-target.swift
@@ -32,3 +32,31 @@ class Solution {
         return result
     }
 }
+/**
+ * https://leetcode.com/problems/all-paths-from-source-to-target/
+ * 
+ * 
+ */ 
+// Date: Fri Jul 24 09:44:37 PDT 2020
+class Solution {
+    /// Comparing with previous submission, remove `visited`
+    /// Since, this is an acyclic graph, which means there is no cycle, and we don't need to keep track of where has been visited. 
+    func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+        var result: [[Int]] = []
+        let n = graph.count - 1
+        func dfs(_ current: Int, _ path: inout [Int]) {
+            if current == n {
+                result.append(path + [current])
+                return
+            }
+            path.append(current)
+            for dst in graph[current] {
+                dfs(dst, &path)
+            }
+            path.removeLast()
+        }
+        var path: [Int] = []
+        dfs(0, &path)
+        return result
+    }
+}
