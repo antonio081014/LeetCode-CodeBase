32366ad75b3ff83166112281c0b6635cc7ddf499
Antonio081014 <antonio081014@antonio081014.com>
2020-07-30 10:44:28 -0700

    Add word-break.swift

--- Swift/word-break.swift
+++ Swift/word-break.swift
@@ -35,3 +35,33 @@ class Solution {
         }
     }
 }
+/**
+ * https://leetcode.com/problems/word-break/
+ * 
+ * 
+ */ 
+// Date: Thu Jul 30 10:44:17 PDT 2020
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(n^2), n is the length of string s.
+    ///     - Space: O(n), n is the length of string s.
+    ///
+    func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+        let dict = Set(wordDict)
+        let s = Array(s)
+        var valid = Array(repeating: false, count: s.count + 1)
+        valid[0] = true
+        for end in 1 ... s.count {
+            // print("End in: \(end)")
+            for start in stride(from: end - 1, through: 0, by: -1) {
+                let word = String(s[start ..< end])
+                // print("---start from: \(start) with \(word)")
+                if valid[start], dict.contains(word) {
+                    valid[end] = true
+                    break
+                }
+            }
+        }
+        return valid[s.count]
+    }
+}
