3d3b7657f808d40d9dcae516718425ec4d1cb516
Antonio081014 <antonio081014@antonio081014.com>
2020-07-31 09:38:20 -0700

    Add climbing-stairs.swift

--- Swift/climbing-stairs.swift
+++ Swift/climbing-stairs.swift
@@ -18,3 +18,54 @@ class Solution {
         return one
     }
 }
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ * 
+ * 
+ */ 
+// Date: Fri Jul 31 09:38:09 PDT 2020
+class Solution {
+    /// Recursive solution with memorization to reduce duplicate calculations.
+    /// - Complexity:
+    ///     - Time: O(n), basically calculate every value smaller than or equal to n.
+    ///     - Space: O(n), use a dictionary to store the calcuated value.
+    ///
+    var dict: [Int : Int] = [:]
+    func climbStairs(_ n: Int) -> Int {
+        if let value = dict[n] { return value }
+        if n == 1 {
+            dict[n] = 1
+            return 1
+        }
+        if n == 0 {
+            dict[n] = 1
+            return 1
+        }
+        let v = climbStairs(n - 1) + climbStairs(n - 2)
+        dict[n] = v
+        return v
+    }
+}
+
+class Solution {
+    /// Iterative way to calculate with DP.
+    /// Uses ways to store the calculated value.
+    /// - Complexity:
+    ///     - Time: O(n), try to calculate 1 to n values.
+    ///     - Space: O(n), try to store the calculated value for 0 to n.
+    ///
+    func climbStairs(_ n: Int) -> Int {
+        var ways = [1]
+        for x in 1 ... n {
+            var sum = 0
+            if x >= 1 {
+                sum += ways[x - 1]
+            }
+            if x >= 2 {
+                sum += ways[x - 2]
+            }
+            ways.append(sum)
+        }
+        return ways[n]
+    }
+}
