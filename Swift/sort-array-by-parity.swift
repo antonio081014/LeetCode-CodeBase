128b8b23f8b2609980c58a5d242c4b3ac460d4c5
Antonio081014 <antonio081014@antonio081014.com>
2020-08-21 10:31:04 -0700

    Add sort-array-by-parity.swift

--- Swift/sort-array-by-parity.swift
+++ Swift/sort-array-by-parity.swift
@@ -30,4 +30,33 @@ class Solution {
         }
         return result
     }
+}/**
+ * https://leetcode.com/problems/sort-array-by-parity/
+ * 
+ * 
+ */ 
+// Date: Fri Aug 21 10:30:54 PDT 2020
+
+/// Using two pointers
+/// The problem asks to have all the even number before odd numbers, 
+/// but it doesn't say to keep in place order.
+///
+/// - Complexity:
+///     - Time: O(n), n is number of elements in A.
+///     - Space: O(n), to make original array mutating. For the rest of memory are constants.
+///
+class Solution {
+    func sortArrayByParity(_ A: [Int]) -> [Int] {
+        var A = A
+        var start = 0
+        var end = A.count - 1
+        while start < end {
+            if A[start] % 2 > A[end] % 2 {
+                A.swapAt(start, end)
+            }
+            if A[start] % 2 == 0 { start += 1 }
+            if A[end] % 2 == 1 { end -= 1 }
+        }
+        return A
+    }
 }
