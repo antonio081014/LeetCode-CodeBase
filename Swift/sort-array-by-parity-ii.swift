93b43cbc829cc50188aad9f5bb1cdb8c42c42511
Antonio081014 <antonio081014@antonio081014.com>
2020-08-28 10:14:19 -0700

    Add sort-array-by-parity-ii.swift

--- Swift/sort-array-by-parity-ii.swift
+++ Swift/sort-array-by-parity-ii.swift
@@ -27,4 +27,32 @@ class Solution {
         }
         return result
     }
+}/**
+ * https://leetcode.com/problems/sort-array-by-parity-ii/
+ * 
+ * 
+ */ 
+// Date: Fri Aug 28 10:14:09 PDT 2020
+/// One pass with two pointers, which keeps the next to be 
+/// located index of odd/even number.
+/// - Complexity: 
+///     - Time: O(n), n is the number of elemetns in A.
+///     - Space: O(n), n is the number of elemetns in A.
+///
+class Solution {
+    func sortArrayByParityII(_ A: [Int]) -> [Int] {
+        var result = Array(repeating: 0, count: A.count)
+        var index1 = 0
+        var index2 = 1
+        for n in A {
+           if n % 2 == 0 {
+               result[index1] = n
+               index1 += 2
+           } else {
+               result[index2] = n
+               index2 += 2
+           }
+        }
+        return result
+    }
 }
