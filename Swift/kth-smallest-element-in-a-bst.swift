9dd0e039c86926f6ab8aa1dede9c6d6fad328fc1
Antonio081014 <antonio081014@antonio081014.com>
2020-09-01 17:25:39 -0700

    Add kth-smallest-element-in-a-bst.swift

--- Swift/kth-smallest-element-in-a-bst.swift
+++ Swift/kth-smallest-element-in-a-bst.swift
@@ -87,3 +87,51 @@ class Solution {
         }
     }
 }
+/**
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ * 
+ * 
+ */ 
+// Date: Tue Sep  1 17:25:29 PDT 2020
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func kthSmallest(_ root: TreeNode?, _ k: Int) -> Int {
+        var result: Int = 0
+        var count = k
+        guard let root = root else { return -1 }
+        self.inorder(root, &count, &result)
+        return result
+    }
+
+    private func inorder(_ root: TreeNode, _ k: inout Int, _ result: inout Int) {
+        /// This line will stop traverse, pruning.
+        if k < 0 { return }
+        if let left = root.left {
+            self.inorder(left, &k, &result)
+        }
+        k -= 1
+        // print("\(root.val) : \(k)")
+        if k == 0 {
+            result = root.val
+            return
+        }
+
+        if let right = root.right {
+            self.inorder(right, &k, &result)
+        }
+    }
+}
