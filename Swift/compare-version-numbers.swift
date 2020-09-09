1cb4515e4f46cbd1e24d0a1d8d081ba2246a1da0
Antonio081014 <antonio081014@antonio081014.com>
2020-09-09 08:06:24 -0700

    Add compare-version-numbers.swift

--- Swift/compare-version-numbers.swift
+++ Swift/compare-version-numbers.swift
@@ -22,3 +22,39 @@ class Solution {
         return 0
     }
 }
+/**
+ * https://leetcode.com/problems/compare-version-numbers/
+ * 
+ * 
+ */ 
+// Date: Wed Sep  9 08:06:14 PDT 2020
+class Solution {
+    /// - Complexity:
+    ///     - Time Complexity: O(max(n, m))
+    ///     - Space Complexity: O(max(n, m))
+    func compareVersion(_ version1: String, _ version2: String) -> Int {
+        // - Time Complexity: O(n)
+        // - Space Complexity: O(n)
+        var v1 = version1.split(separator: ".").map { Int($0)! }
+        // - Time Complexity: O(m)
+        // - Space Complexity: O(m)
+        var v2 = version2.split(separator: ".").map { Int($0)! }
+        let n = max(v1.count, v2.count)
+        // - Space Complexity: O(max(n, m))
+        if v1.count < n {
+            v1 += Array(repeating: 0, count: n - v1.count)
+        } else {
+            v2 += Array(repeating: 0, count: n - v2.count)
+        }
+        var index = 0
+        // - Time Complexity: O(max(n, m))
+        while index < v1.count {
+            if v1[index] != v2[index] {
+                if v1[index] < v2[index] { return -1 }
+                return 1
+            }
+            index += 1
+        }
+        return 0
+    }
+}
