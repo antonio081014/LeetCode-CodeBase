275eb62f4be7617db803419483d21a6fa2837940
Antonio081014 <antonio081014@antonio081014.com>
2020-09-23 15:52:54 -0700

    Add comments to gas-station.swift

--- Swift/gas-station.swift
+++ Swift/gas-station.swift
@@ -4,6 +4,13 @@
  * 
  */ 
 // Date: Wed Sep 23 15:37:58 PDT 2020
+/// The idea is
+/// 1. If the total gas - cost is greater or equal to 0, there will be a solution for sure.
+/// 2. Then, to find the start station,
+///     2.1 Start from station 0, sum all the gas - cost would be available in tank.
+///     2.2 If at station `index`, the gas in tank is smaller than 0, then next station is not reachable.
+///     2.3 Thus, from start to `index` is not a reachable path.
+///     2.4 So, we start with next index, which is `index + 1`.
 class Solution {
     func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
         var tank = 0
