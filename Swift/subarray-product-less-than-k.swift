847341cafe3a4306111eed53550602b1a2976d77
Antonio081014 <antonio081014@antonio081014.com>
2020-09-29 11:21:04 -0700

    Add subarray-product-less-than-k.swift

--- Swift/subarray-product-less-than-k.swift
+++ Swift/subarray-product-less-than-k.swift
@@ -27,4 +27,43 @@ class Solution {
         }
         return count
     }
+}/**
+ * https://leetcode.com/problems/subarray-product-less-than-k/
+ * 
+ * 
+ */ 
+// Date: Tue Sep 29 11:20:53 PDT 2020
+/// Binary Search on Logarithms
+/// We can reduce the problem to subarray sums instead of subarray products. 
+/// The motivation for this is that the product of some arbitrary subarray may be way too large (potentially 1000^50000), 
+/// and also dealing with sums gives us some more familiarity as it becomes similar to other problems we may have solved before.
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(nlogn), n is the length of `nums`
+    ///     - Space: O(n)
+    func numSubarrayProductLessThanK(_ nums: [Int], _ k: Int) -> Int {
+        if k <= 1 { return 0 }
+        let k = log(Double(k))
+        var prod = [0.0]
+        for n in nums {
+            prod.append((prod.last ?? 0.0) + log(Double(n)))
+        }
+
+        var count = 0
+        for end in 1 ... nums.count {
+            var left = 0
+            var right = end
+            while left < right {
+                let mid = left + (right - left) / 2
+                if prod[end] - prod[mid] >= k - 1e-9 {
+                    left = mid + 1
+                } else {
+                    right = mid
+                }
+            }
+            count += end - left
+        }
+        return count
+    }
 }
+
