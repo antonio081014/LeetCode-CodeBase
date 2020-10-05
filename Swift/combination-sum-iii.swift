a03a617197ad7f08b2a04512b06747afde609761
Antonio081014 <antonio081014@antonio081014.com>
2020-10-05 16:18:30 -0700

    Add combination-sum-iii.swift

--- Swift/combination-sum-iii.swift
+++ Swift/combination-sum-iii.swift
@@ -27,4 +27,37 @@ class Solution {
         dfs([], 0)
         return result
     }
+}/**
+ * https://leetcode.com/problems/combination-sum-iii/
+ * 
+ * 
+ */ 
+// Date: Mon Oct  5 16:18:19 PDT 2020
+class Solution {
+    /// BFS
+    /// Start with empty list and sum 0.
+    /// Then, try to add a number `next`, which is greater than the last one in the list.
+    /// iff sum + next <= n.
+    ///
+    /// - Complexity:
+    ///     - Time: O(10 * n), 10 is 0 to 9.
+    func combinationSum3(_ k: Int, _ n: Int) -> [[Int]] {
+        var queue: [(Int, [Int])] = [(0, [])]
+        var result: [[Int]] = []
+        
+        while queue.isEmpty == false {
+            let (sum, list) = queue.removeFirst()
+            if list.count == k {
+                if sum == n { result.append(list) }
+                continue
+            }
+            let last = list.last ?? 0
+            for next in stride(from: last + 1, to: 10, by: 1) {
+                if sum + next <= n {
+                    queue.append((sum + next, list + [next]))
+                }
+            }
+        }
+        return result
+    }
 }
