bd4963cb7d9c180ba6d253bfc6a4e427a42a0ef9
Antonio081014 <antonio081014@antonio081014.com>
2020-10-30 15:51:23 -0700

    Add 132-pattern.swift

--- Swift/132-pattern.swift
+++ Swift/132-pattern.swift
@@ -22,3 +22,38 @@ class Solution {
 }
 
 
+/**
+ * https://leetcode.com/problems/132-pattern/
+ * 
+ * 
+ */ 
+// Date: Fri Oct 30 15:51:12 PDT 2020
+class Solution {
+    /// - Complexity: 
+    ///     - Time: O(n), n is nums.count
+    ///     - Space: O(n), to store the leftMin and possible right ends in stack.
+    func find132pattern(_ nums: [Int]) -> Bool {
+        let n = nums.count
+        var leftMin = nums
+        for index in stride(from: 1, to: n, by: 1) {
+            leftMin[index] = min(leftMin[index - 1], nums[index])
+        }
+        
+        var stack = [Int]()
+        for index in stride(from: n - 1, through: 0, by: -1) {
+            if nums[index] > leftMin[index] {
+                // We see 1 and 3, seeking 2.
+                while let last = stack.last, last <= leftMin[index] {
+                    stack.removeLast()
+                }
+                // Validate the 2.
+                if let last = stack.last, last < nums[index] { return true }
+                // nums[index] > min value, so this might be the 2 for the other tuples.
+                stack.append(nums[index])
+            }
+        }
+        return false
+    }
+}
+
+
