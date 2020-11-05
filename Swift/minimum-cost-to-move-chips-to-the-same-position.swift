177db3c3e8a61d43466274f03d9598ee8ad6ffda
Antonio081014 <antonio081014@antonio081014.com>
2020-11-05 10:01:20 -0800

    Add minimum-cost-to-move-chips-to-the-same-position.swift

--- Swift/minimum-cost-to-move-chips-to-the-same-position.swift
+++ Swift/minimum-cost-to-move-chips-to-the-same-position.swift
@@ -14,4 +14,15 @@ class Solution {
         }
         return min(odds, even)
     }
+}/**
+ * https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+ * 
+ * 
+ */ 
+// Date: Thu Nov  5 10:01:09 PST 2020
+class Solution {
+    func minCostToMoveChips(_ position: [Int]) -> Int {
+        let count = position.filter { $0 % 2 == 0}.count
+        return min(position.count - count, count)
+    }
 }
