e5af5791098da69a5a89c9e1ecd0db4f4797f65e
Antonio081014 <antonio081014@antonio081014.com>
2020-11-29 17:17:37 -0800

    Add numbers-at-most-n-given-digit-set.swift

--- Swift/numbers-at-most-n-given-digit-set.swift
+++ Swift/numbers-at-most-n-given-digit-set.swift
@@ -26,3 +26,39 @@ class Solution {
     }
 }
 
+/**
+ * https://leetcode.com/problems/numbers-at-most-n-given-digit-set/
+ * 
+ * 
+ */ 
+// Date: Sun Nov 29 17:17:26 PST 2020
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(log10(n))
+    ///     - Space: O(1)
+    ////
+    /// Reference: [Tech blog of HuaHua](https://zxi.mytechroad.com/blog/math/leetcode-902-numbers-at-most-n-given-digit-set/)
+    ///
+    func atMostNGivenDigitSet(_ digits: [String], _ n: Int) -> Int {
+        let n = Array(String(n)).map { String($0) }
+        var result = 0
+        for index in 1 ..< n.count {
+            result += Int(pow(Double(digits.count), Double(index)))
+        }
+
+        for index in 0 ..< n.count {
+            var exists = false
+            for d in digits {
+                if d < n[index] {
+                    result += Int(pow(Double(digits.count), Double(n.count - 1 - index)))
+                } else if d == n[index] {
+                    exists = true
+                    break
+                }
+            }
+            if exists == false { return result }
+        }
+        return result + 1
+    }
+}
+
