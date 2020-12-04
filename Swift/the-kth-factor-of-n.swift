a8f4bade84ca7f8480934a2d4292962acde74452
Antonio081014 <antonio081014@antonio081014.com>
2020-12-04 10:19:02 -0800

    Add the-kth-factor-of-n.swift

--- Swift/the-kth-factor-of-n.swift
+++ Swift/the-kth-factor-of-n.swift
@@ -15,4 +15,28 @@ class Solution {
         }
         return -1
     }
+}/**
+ * https://leetcode.com/problems/the-kth-factor-of-n/
+ * 
+ * 
+ */ 
+// Date: Fri Dec  4 10:18:52 PST 2020
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(sqrt(n))
+    ///     - Space: O(sqrt(n))
+    func kthFactor(_ n: Int, _ k: Int) -> Int {
+        var count = 0
+        var list: [Int] = []
+        var d = 1
+        while d * d <= n {
+            if n % d == 0 {
+                count += 1
+                if d * d != n { list.append(n / d) }
+                if count == k { return d }
+            }
+            d += 1
+        }
+        return list.count + count < k ? -1 : list[list.count - k + count]
+    }
 }
