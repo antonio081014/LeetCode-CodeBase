1502f0e51148a31118575b99e7169e22211d1f9e
Antonio081014 <antonio081014@antonio081014.com>
2020-12-14 17:30:24 -0800

    Add palindrome-partitioning.swift

--- Swift/palindrome-partitioning.swift
+++ Swift/palindrome-partitioning.swift
@@ -36,3 +36,34 @@ class Solution {
     }
 }
 
+/**
+ * https://leetcode.com/problems/palindrome-partitioning/
+ * 
+ * 
+ */ 
+// Date: Mon Dec 14 17:30:13 PST 2020
+class Solution {
+    func partition(_ s: String) -> [[String]] {
+        let s = Array(s)
+        var result = [[String]]()
+        var isPalindrome = Array(repeating: Array(repeating: false, count: s.count), count: s.count)
+        
+        func dfs(_ start: Int, _ list: [String]) {
+            if start >= s.count {
+                result.append(list)
+                return
+            }
+            for end in stride(from: start, to: s.count, by: 1) {
+                if s[start] == s[end] && (end - start <= 2 || isPalindrome[start + 1][end - 1]) {
+                    isPalindrome[start][end] = true
+                    dfs(end + 1, list + [String(s[start ... end])])
+                }
+            }
+
+        }
+        
+        dfs(0, [])
+        return result
+    }
+}
+
