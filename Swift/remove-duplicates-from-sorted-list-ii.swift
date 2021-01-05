737a343744defa0214d518194f2aff967e8de1fd
Antonio081014 <antonio081014@antonio081014.com>
2021-01-05 09:56:32 -0800

    Add remove-duplicates-from-sorted-list-ii.swift

--- Swift/remove-duplicates-from-sorted-list-ii.swift
+++ Swift/remove-duplicates-from-sorted-list-ii.swift
@@ -42,3 +42,42 @@ class Solution {
         return root.next
     }
 }
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+ * 
+ * 
+ */ 
+// Date: Tue Jan  5 09:56:18 PST 2021
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        let root = ListNode(0)
+        root.next = head
+        var node = head
+        var preNode: ListNode? = root
+        while node != nil {
+            var dups = false
+            while node?.next != nil, node?.val == node?.next?.val {
+                node = node?.next
+                dups = true
+            }
+            if dups {
+                preNode?.next = node?.next
+            } else {
+                preNode = preNode?.next
+            }
+            node = node?.next
+        }
+        return root.next
+    }
+}
