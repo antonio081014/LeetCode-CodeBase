b5312f3bc34d7b2fa39f419b3e9824883c32b323
Antonio081014 <antonio081014@antonio081014.com>
2021-01-17 09:30:52 -0800

    Add count-sorted-vowel-strings.swift

--- Swift/count-sorted-vowel-strings.swift
+++ Swift/count-sorted-vowel-strings.swift
@@ -15,4 +15,31 @@ class Solution {
         }
         return (a+e+i+o+u)
     }
+}/**
+ * https://leetcode.com/problems/count-sorted-vowel-strings/
+ * 
+ * 
+ */ 
+// Date: Sun Jan 17 09:30:41 PST 2021
+class Solution {
+    func countVowelStrings(_ n: Int) -> Int {
+        var dp = Array(repeating: Array(repeating: 0, count: 5), count: n + 1)
+        for x in 0 ..< 5 {
+            dp[0][x] = 1
+        }
+        /// j:
+        ///     0 -> 'u'
+        ///     1 -> 'o'
+        ///     2 -> 'i'
+        ///     3 -> 'e'
+        ///     4 -> 'a'
+        for x in 1 ... n {
+            dp[x][0] = dp[x - 1][0]
+            for j in 1 ..< 5 {
+                dp[x][j] = dp[x - 1][j] + dp[x][j - 1]
+            }
+        }
+        // print(dp)
+        return dp[n][4]
+    }
 }
