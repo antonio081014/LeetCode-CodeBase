2900d0347ef9d8c90a7f59c9078cb573d773a0c1
Antonio081014 <antonio081014@antonio081014.com>
2021-01-27 14:07:20 -0800

    Update concatenation-of-consecutive-binary-numbers.swift

--- Swift/concatenation-of-consecutive-binary-numbers.swift
+++ Swift/concatenation-of-consecutive-binary-numbers.swift
@@ -8,8 +8,8 @@ class Solution {
     // f(n) = f(n-1) * 2^(n_binary_length) + n
     func concatenatedBinary(_ n: Int) -> Int {
         let d = 1_000_000_007
-        var x = 1
-        for i in stride(from: 2, through: n, by: 1) {
+        var x = 0
+        for i in stride(from: 1, through: n, by: 1) {
             x = ((x << (i.bitWidth - i.leadingZeroBitCount)) + i) % d
         }
         return x
