df4c7a636f0b2a22000f951c301616ce1a9b0671
Antonio081014 <antonio081014@antonio081014.com>
2021-02-05 09:31:58 -0800

    Add simplify-path.swift

--- Swift/simplify-path.swift
+++ Swift/simplify-path.swift
@@ -24,3 +24,29 @@ class Solution {
         return "/" + ret.joined(separator: "/")
     }
 }
+/**
+ * https://leetcode.com/problems/simplify-path/
+ * 
+ * 
+ */ 
+// Date: Fri Feb  5 09:31:47 PST 2021
+class Solution {
+    func simplifyPath(_ path: String) -> String {
+        let list = path.components(separatedBy: "/")
+        var result: [String] = []
+        for p in list {
+            if p.count > 0 {
+                if p == ".." {
+                    if result.isEmpty == false {
+                        result.removeLast()
+                    }
+                } else if p == "." {
+                    continue
+                } else {
+                    result.append(p)
+                }
+            }
+        }
+        return "/" + result.joined(separator: "/")
+    }
+}
