2d5ccf54992d5da25e413049c25532260bbfbd21
Antonio081014 <antonio081014@antonio081014.com>
2021-02-08 09:48:29 -0800

    Add peeking-iterator.swift

--- Swift/peeking-iterator.swift
+++ Swift/peeking-iterator.swift
@@ -36,4 +36,63 @@ class PeekingIterator {
  * let ret_1: Int = obj.next()
  * let ret_2: Int = obj.peek()
  * let ret_3: Bool = obj.hasNext()
+ *//**
+ * https://leetcode.com/problems/peeking-iterator/
+ * 
+ * 
+ */ 
+// Date: Mon Feb  8 09:48:18 PST 2021
+// Swift IndexingIterator refernence:
+// https://developer.apple.com/documentation/swift/indexingiterator
+
+class PeekingIterator {
+    var list: PeekingIteratorGeneric<Array<Int>>
+    init(_ arr: IndexingIterator<Array<Int>>) {
+        self.list = PeekingIteratorGeneric(arr)
+    }
+
+    func next() -> Int {
+        return self.list.next()
+    }
+
+    func peek() -> Int {
+        return self.list.peek()
+    }
+
+    func hasNext() -> Bool {
+        return self.list.hasNext()
+    }
+}
+
+class PeekingIteratorGeneric<T: Collection> {
+    var elements: IndexingIterator<T>
+    var nextElement: T.Element?
+
+    init(_ arr: IndexingIterator<T>) {
+        self.elements = arr
+        self.nextElement = self.elements.next()
+    }
+
+    func next() -> T.Element {
+        guard let element = self.nextElement else { fatalError() }
+        self.nextElement = self.elements.next()
+        return element
+    }
+
+    func peek() -> T.Element {
+        guard let element = self.nextElement else { fatalError() }
+        return element
+    }
+
+    func hasNext() -> Bool {
+        return self.nextElement != nil
+    }
+}
+
+/**
+ * Your PeekingIterator object will be instantiated and called as such:
+ * let obj = PeekingIterator(arr)
+ * let ret_1: Int = obj.next()
+ * let ret_2: Int = obj.peek()
+ * let ret_3: Bool = obj.hasNext()
  */
