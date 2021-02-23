32f6d4fe09fdbc69cf7c028bef0c1096ef4a86c5
Antonio081014 <antonio081014@antonio081014.com>
2021-02-23 08:22:59 -0800

    Update search-a-2d-matrix-ii.swift

--- Swift/search-a-2d-matrix-ii.swift
+++ Swift/search-a-2d-matrix-ii.swift
@@ -17,3 +17,21 @@ class Solution {
         return false
     }
 }
+
+class Solution {
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        if matrix.count == 0 || matrix[0].count == 0 { return false }
+        var row = 0
+        var col = matrix[0].count - 1
+        while row < matrix.count, col >= 0 {
+            if matrix[row][col] == target {
+                return true
+            } else if matrix[row][col] > target {
+                col -= 1
+            } else {
+                row += 1
+            }
+        }
+        return false
+    }
+}
