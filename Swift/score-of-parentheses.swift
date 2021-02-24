918b525cfbdacd8c0342c97e66743d7348177a8f
Antonio081014 <antonio081014@antonio081014.com>
2021-02-24 10:51:03 -0800

    Add score-of-parentheses.swift

--- Swift/score-of-parentheses.swift
+++ Swift/score-of-parentheses.swift
@@ -20,3 +20,27 @@ class Solution {
     }
 }
 
+/**
+ * https://leetcode.com/problems/score-of-parentheses/
+ * 
+ * 
+ */ 
+// Date: Wed Feb 24 10:50:53 PST 2021
+class Solution {
+    func scoreOfParentheses(_ S: String) -> Int {
+        var depth = 0
+        var result = 0
+        let S = Array(S)
+        for index in 0 ..< S.count {
+            if S[index] == Character("(") {
+                depth += 1
+            } else {
+                depth -= 1
+                if S[index - 1] == Character("(") {
+                    result += 1 << depth
+                }
+            }
+        }
+        return result
+    }
+}
