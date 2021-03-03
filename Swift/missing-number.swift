e1f4f77f5be96a8de123a4ca3ea976b6028b0a3f
Antonio081014 <antonio081014@antonio081014.com>
2021-03-03 15:44:33 -0800

    Add missing-number.swift

--- Swift/missing-number.swift
+++ Swift/missing-number.swift
@@ -14,3 +14,19 @@ class Solution {
         return total
     }
 }
+/**
+ * https://leetcode.com/problems/missing-number/
+ * 
+ * 
+ */ 
+// Date: Wed Mar  3 15:44:22 PST 2021
+class Solution {
+    func missingNumber(_ nums: [Int]) -> Int {
+        let n = nums.count
+        var sum = (1 + n) * n / 2
+        for x in nums {
+            sum -= x
+        }
+        return sum
+    }
+}
