a29b6d13e078cb11d189ef1cefb4c36e6b0461de
Antonio081014 <antonio081014@antonio081014.com>
2021-03-09 15:37:53 -0800

    Add add-one-row-to-tree.swift

--- Swift/add-one-row-to-tree.swift
+++ Swift/add-one-row-to-tree.swift
@@ -112,4 +112,56 @@ class Solution {
         }
         return root
     }
+}/**
+ * https://leetcode.com/problems/add-one-row-to-tree/
+ * 
+ * 
+ */ 
+// Date: Tue Mar  9 15:37:42 PST 2021
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func addOneRow(_ root: TreeNode?, _ v: Int, _ d: Int) -> TreeNode? {
+        if d == 1 {
+            return TreeNode(v, root, nil)
+        }
+        guard let root = root else { return nil }
+        var queue = [root]
+        var depth = [1]
+        var toUpdateList: [TreeNode] = []
+        while queue.isEmpty == false {
+            let node = queue.removeLast()
+            let level = depth.removeLast()
+            if level + 1 < d {
+                if let left = node.left {
+                    queue.append(left)
+                    depth.append(level + 1)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                    depth.append(level + 1)
+                }
+            } else {
+                toUpdateList.append(node)
+            }
+        }
+        for node in toUpdateList {
+            node.left = TreeNode(v, node.left, nil)
+            node.right = TreeNode(v, nil, node.right)
+        }
+        return root
+    }
 }
