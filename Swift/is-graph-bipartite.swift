90344c551c238045e1abd18f79c117b218d1c8b9
Antonio081014 <antonio081014@antonio081014.com>
2021-03-10 10:36:23 -0800

    Add is-graph-bipartite.swift

--- Swift/is-graph-bipartite.swift
+++ Swift/is-graph-bipartite.swift
@@ -33,4 +33,38 @@ class Solution {
         }
         return true
     }
+}/**
+ * https://leetcode.com/problems/is-graph-bipartite/
+ * 
+ * 
+ */ 
+// Date: Wed Mar 10 10:36:12 PST 2021
+class Solution {
+    func isBipartite(_ graph: [[Int]]) -> Bool {
+        let n = graph.count
+        var color = Array(repeating: 0, count: n)
+        for start in 0 ..< n {
+            if color[start] == 0 {
+                color[start] = 1
+                var queue = [start]
+                while queue.isEmpty == false {
+                    var size = queue.count
+                    while size > 0 {
+                        size -= 1
+                        let node = queue.removeFirst()
+                        let c = color[node]
+                        for next in graph[node] {
+                            if color[next] == 0 {
+                                color[next] = 3 - c
+                                queue.append(next)
+                            } else if color[next] == c {
+                                return false
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    }
 }
