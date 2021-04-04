f6df7af853e5f1609a7813352882eadbb80ebe31
Antonio081014 <antonio081014@antonio081014.com>
2021-04-04 10:00:04 -0700

    Add design-circular-queue.swift

--- Swift/design-circular-queue.swift
+++ Swift/design-circular-queue.swift
@@ -60,4 +60,65 @@ class MyCircularQueue {
  * let ret_4: Int = obj.Rear()
  * let ret_5: Bool = obj.isEmpty()
  * let ret_6: Bool = obj.isFull()
+ *//**
+ * https://leetcode.com/problems/design-circular-queue/
+ * 
+ * 
+ */ 
+// Date: Sun Apr  4 09:59:53 PDT 2021
+class MyCircularQueue {
+    let capacity: Int
+    var array: [Int]
+    var top: Int
+    var count: Int
+    init(_ k: Int) {
+        self.capacity = k
+        self.array = Array(repeating: 0, count: k)
+        self.top = 0
+        self.count = 0
+    }
+    
+    func enQueue(_ value: Int) -> Bool {
+        if self.isFull() { return false }
+        self.array[(self.top + self.count) % self.capacity] = value
+        self.count += 1
+        return true
+    }
+    
+    func deQueue() -> Bool {
+        if self.isEmpty() { return false }
+        self.top = (self.top + 1) % self.capacity
+        self.count -= 1
+        return true
+    }
+    
+    func Front() -> Int {
+        if self.isEmpty() { return -1 }
+        return self.array[self.top]
+    }
+    
+    func Rear() -> Int {
+        if self.isEmpty() { return -1 }
+        let index = (self.top + self.count - 1 + self.capacity) % self.capacity
+        return self.array[index]
+    }
+    
+    func isEmpty() -> Bool {
+        return self.count == 0
+    }
+    
+    func isFull() -> Bool {
+        return self.count == self.capacity
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * let obj = MyCircularQueue(k)
+ * let ret_1: Bool = obj.enQueue(value)
+ * let ret_2: Bool = obj.deQueue()
+ * let ret_3: Int = obj.Front()
+ * let ret_4: Int = obj.Rear()
+ * let ret_5: Bool = obj.isEmpty()
+ * let ret_6: Bool = obj.isFull()
  */
