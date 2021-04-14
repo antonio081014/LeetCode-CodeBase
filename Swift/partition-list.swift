bd049136b6793cc6271ae5248cad1899c0ab514d
Antonio081014 <antonio081014@antonio081014.com>
2021-04-14 15:09:43 -0700

    Add partition-list.swift

--- Swift/partition-list.swift
+++ Swift/partition-list.swift
@@ -44,3 +44,40 @@ class Solution {
         return smallHead.next
     }
 }
+/**
+ * https://leetcode.com/problems/partition-list/
+ * 
+ * 
+ */ 
+// Date: Wed Apr 14 15:09:33 PDT 2021
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
+        let result1 = ListNode()
+        let result2 = ListNode()
+        var node = head
+        var smallNode = result1
+        var largeNode = result2
+        while node != nil {
+            if node!.val < x {
+                smallNode.next = ListNode(node!.val)
+                smallNode = smallNode.next!
+            } else {
+                largeNode.next = ListNode(node!.val)
+                largeNode = largeNode.next!
+            }
+            node = node?.next
+        }
+        smallNode.next = result2.next
+        return result1.next
+    }
+}
