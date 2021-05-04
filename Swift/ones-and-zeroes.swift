e02bf7a33877c58d23becbfad3eca54eb676ada0
Antonio081014 <antonio081014@antonio081014.com>
2021-05-04 15:03:20 -0700

    Add ones-and-zeroes.swift

--- Swift/ones-and-zeroes.swift
+++ Swift/ones-and-zeroes.swift
@@ -36,4 +36,42 @@ class Solution {
 
         return dp[l][m][n]
     }
+}/**
+ * https://leetcode.com/problems/ones-and-zeroes/
+ * 
+ * 
+ */ 
+// Date: Tue May  4 15:03:10 PDT 2021
+class Solution {
+    func findMaxForm(_ strs: [String], _ m: Int, _ n: Int) -> Int {
+        let l = strs.count
+        var dp = Array(repeating: Array(repeating: Array(repeating: 0, count: n + 1), count: m + 1), count: 2)        
+        for index in 1 ... l {
+            let (x, y) = calculate(strs[index - 1])
+            for a in 0 ... m {
+                for b in 0 ... n {
+                    if a >= x, b >= y {
+                        dp[index % 2][a][b] = max(dp[1 - index % 2][a][b], dp[1 - index % 2][a - x][b - y] + 1)
+                    } else {
+                        dp[index % 2][a][b] = dp[1 - index % 2][a][b]
+                    }
+                }
+            }
+        }
+
+        return dp[l % 2][m][n]
+    }
+
+    private func calculate(_ text: String) -> (Int, Int) {
+        var a = 0
+        var b = 0
+        for c in text {
+            if c == Character("0") {
+                a += 1
+            } else {
+                b += 1
+            }
+        }
+        return (a, b)
+    }
 }
