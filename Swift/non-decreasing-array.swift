38c51d15b15ed6d7a06fec4b81851947c815b0d4
Antonio081014 <antonio081014@antonio081014.com>
2021-05-04 09:21:52 -0700

    Add non-decreasing-array.swift

--- Swift/non-decreasing-array.swift
+++ Swift/non-decreasing-array.swift
@@ -19,4 +19,30 @@ class Solution {
         }
         return a == a.sorted() || b == b.sorted()
     }
+}/**
+ * https://leetcode.com/problems/non-decreasing-array/
+ * 
+ * 
+ */ 
+// Date: Tue May  4 09:21:41 PDT 2021
+class Solution {
+    func checkPossibility(_ nums: [Int]) -> Bool {
+        var nums = nums
+        var count = 0
+        for index in stride(from: 1, to: nums.count, by: 1) {
+            if nums[index - 1] > nums[index] {
+                count += 1
+                // 
+                //  /\            /\
+                //    \          /
+                //
+                if index < 2 || nums[index - 2] <= nums[index] {
+                    nums[index - 1] = nums[index]
+                } else {
+                    nums[index] = nums[index - 1]
+                }
+            }
+        }
+        return count <= 1
+    }
 }
