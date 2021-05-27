3362f9f14078c286e4449abf23a63de7f94dc89c
Antonio081014 <antonio081014@antonio081014.com>
2021-05-27 08:49:43 -0700

    Add maximum-product-of-word-lengths.swift

--- Swift/maximum-product-of-word-lengths.swift
+++ Swift/maximum-product-of-word-lengths.swift
@@ -26,4 +26,37 @@ class Solution {
         }
         return result
     }
+}/**
+ * https://leetcode.com/problems/maximum-product-of-word-lengths/
+ * 
+ * 
+ */ 
+// Date: Thu May 27 08:49:32 PDT 2021
+class Solution {
+    func maxProduct(_ words: [String]) -> Int {
+        var map: [Set<Character> : String] = [:]
+        for word in words {
+            var set: Set<Character> = []
+            for c in word {
+                set.insert(c)
+            }
+            if let w = map[set] {
+                map[set] = w.count > word.count ? w : word
+            } else {
+                map[set] = word
+            }
+        }
+        var result = 0
+        let keys = Array(map.keys)
+        for index1 in stride(from: 1, to: keys.count, by: 1) {
+            let set1 = keys[index1]
+            for index2 in stride(from: 0, to: index1, by: 1) {
+                let set2 = keys[index2]
+                if set1.isDisjoint(with: set2) {
+                    result = max(result, map[set1, default: ""].count * map[set2, default: ""].count)
+                }
+            }
+        }
+        return result
+    }
 }
