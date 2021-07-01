450c33bef8c5991fcb85facf5273a677a19dac8e
Antonio081014 <antonio081014@antonio081014.com>
2021-07-01 10:25:25 -0700

    Add gray-code.swift

--- Swift/gray-code.swift
+++ Swift/gray-code.swift
@@ -17,4 +17,18 @@ class Solution {
         }
         return result
     }
+}/**
+ * https://leetcode.com/problems/gray-code/
+ * 
+ * 
+ */ 
+// Date: Thu Jul  1 10:25:15 PDT 2021
+class Solution {
+    func grayCode(_ n: Int) -> [Int] {
+        if n == 1 {
+            return [0, 1]
+        }
+        let prevList = grayCode(n - 1)
+        return prevList + (prevList.map { $0 + (1 << (n - 1)) }.reversed())
+    }
 }
