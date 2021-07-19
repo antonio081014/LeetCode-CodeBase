6d9dc42f0a30b6bef3c237f66a1b3f39383da5f1
Antonio081014 <antonio081014@antonio081014.com>
2021-07-19 09:17:20 -0700

    Add lowest-common-ancestor-of-a-binary-search-tree.swift

--- Swift/lowest-common-ancestor-of-a-binary-search-tree.swift
+++ Swift/lowest-common-ancestor-of-a-binary-search-tree.swift
@@ -31,4 +31,40 @@ class Solution {
             return lowestCommonAncestor(root.right, p, q)
         }
     }
+}/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * 
+ * 
+ */ 
+// Date: Mon Jul 19 09:17:09 PDT 2021
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        let pv = min(p!.val, q!.val)
+        let qv = max(p!.val, q!.val)
+        
+        var resultNode = root
+        while let node = resultNode {
+            if node.val >= pv, node.val <= qv { return node }
+            if node.val >= qv {
+                resultNode = node.left
+            } else {
+                resultNode = node.right
+            }
+        }
+        return nil
+    }
 }
