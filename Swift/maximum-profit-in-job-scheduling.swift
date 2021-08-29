124f1bef9f8581ab73433966cfeb4e8bddba34a9
Antonio081014 <antonio081014@antonio081014.com>
2021-08-29 15:24:13 -0700

    Add maximum-profit-in-job-scheduling.swift

--- Swift/maximum-profit-in-job-scheduling.swift
+++ Swift/maximum-profit-in-job-scheduling.swift
@@ -47,4 +47,61 @@ class Solution {
         // print(dp)
         return dp[0]
     }
+}/**
+ * https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+ * 
+ * 
+ */ 
+// Date: Sun Aug 29 15:24:03 PDT 2021
+class Solution {
+    struct Job: CustomDebugStringConvertible {
+        let start: Int
+        let end: Int
+        let profit: Int
+
+        var debugDescription: String {
+            return "\(start) - \(end) : \(profit)"
+        }
+    }
+
+    func jobScheduling(_ startTime: [Int], _ endTime: [Int], _ profit: [Int]) -> Int {
+        
+
+        let n = startTime.count
+        var jobList = [Job]()
+        for index in 0 ..< n {
+            jobList.append(Job(start: startTime[index], end: endTime[index], profit: profit[index]))
+        }
+
+        jobList.sort { jobA, jobB in
+            if jobA.start == jobB.start, jobA.end == jobB.end { return jobA.profit < jobB.profit }
+            if jobA.start == jobB.start { return jobA.end < jobB.end }
+            return jobA.start < jobB.start
+        }
+
+        // print(jobList)
+
+        var dp = Array(repeating: 0, count: n)
+        for index in stride(from: n - 1, through: 0, by: -1) {
+            dp[index] = jobList[index].profit
+            if index < n - 1 {
+                dp[index] = max(dp[index], dp[index + 1])
+            }
+            let next = findNext(jobList, from: index)
+            if next < n {
+                dp[index] = max(dp[index], dp[next] + jobList[index].profit)
+            }
+        }
+        // print(dp)
+        return dp[0]
+    }
+
+    private func findNext(_ jobList: [Job], from index: Int) -> Int {
+        let n = jobList.count
+        var next = index + 1
+        while next < n, jobList[next].start < jobList[index].end {
+            next += 1
+        }
+        return next
+    }
 }
