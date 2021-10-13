e7377878c734ad38bc4c8b829f93d905db77fb08
Antonio081014 <antonio081014@antonio081014.com>
2021-10-12 21:24:10 -0700

    Add construct-binary-search-tree-from-preorder-traversal.swift

--- Swift/construct-binary-search-tree-from-preorder-traversal.swift
+++ Swift/construct-binary-search-tree-from-preorder-traversal.swift
@@ -39,3 +39,40 @@ class Solution {
         return root
     }
 }
+/**
+ * https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+ * 
+ * 
+ */ 
+// Date: Tue Oct 12 21:23:59 PDT 2021
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func bstFromPreorder(_ preorder: [Int]) -> TreeNode? {
+        if preorder.isEmpty { return nil }
+        let root = TreeNode(preorder[0])
+        var leftList = [Int]()
+        var rightList = [Int]()
+        for index in stride(from: 1, to: preorder.count, by: 1) {
+            if preorder[index] < preorder[0] {
+                leftList.append(preorder[index])
+            } else {
+                rightList.append(preorder[index])
+            }
+        }
+        root.left = bstFromPreorder(leftList)
+        root.right = bstFromPreorder(rightList)
+        return root
+    }
+}
