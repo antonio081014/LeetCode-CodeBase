eb1b192986f948243991f7fafcc32af0ad87df34
Yi <777705+antonio081014@users.noreply.github.com>
2022-02-11 08:48:03 -0800

    Update permutation-in-string.swift

--- Swift/permutation-in-string.swift
+++ Swift/permutation-in-string.swift
@@ -35,3 +35,29 @@ class Solution {
         return Int(letter.unicodeScalars.first!.value) - Int(Unicode.Scalar("a").value)
     }
 }
+
+
+// Fri Feb 11 08:47:52 PST 2022
+class Solution {
+    func checkInclusion(_ s1: String, _ s2: String) -> Bool {
+        let len1 = s1.count
+        let len2 = s2.count
+        
+        var count1 = [Character : Int]()
+        for c in s1 {
+            count1[c, default: 0] += 1
+        }
+        
+        var count2 = [Character : Int]()
+        let s2 = Array(s2)
+        for index in 0 ..< len2 {
+            let c = s2[index]
+            count2[c, default: 0] += 1
+            if index >= len1, let v = count2[s2[index - len1]] {
+                count2[s2[index - len1]] = v > 1 ? v - 1 : nil
+            }
+            if count1 == count2 { return true }
+        }
+        return false
+    }
+}
