6d4676f23437654f34e5f9761c31454384a1aa4d
Yi <777705+antonio081014@users.noreply.github.com>
2022-03-11 09:20:03 -0800

    Update rotate-list.swift

--- Swift/rotate-list.swift
+++ Swift/rotate-list.swift
@@ -41,3 +41,43 @@ class Solution {
         return result
     }
 }
+
+// Date: Fri Mar 11 09:19:55 PST 2022
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        var count = 0
+        var node = head
+        while node != nil {
+            count += 1
+            node = node?.next
+        }
+        guard k > 0, count > 0 else { return head }
+        
+        let k = k % count
+        count = 0
+        var fast = head
+        var slow = head
+        while fast?.next != nil {
+            if count >= k {
+                slow = slow?.next
+            }
+            count += 1
+            fast = fast?.next
+        }
+        fast?.next = head
+        let root = slow?.next
+        slow?.next = nil
+        return root
+    }
+}
+
