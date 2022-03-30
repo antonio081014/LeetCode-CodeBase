513f3ffb8f62263ae386a6ed7379d2376778d1d0
Yi <777705+antonio081014@users.noreply.github.com>
2022-03-30 10:33:27 -0700

    Update search-a-2d-matrix.swift

--- Swift/search-a-2d-matrix.swift
+++ Swift/search-a-2d-matrix.swift
@@ -20,3 +20,62 @@ class Solution {
         return false
     }
 }
+
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(logm + logn), n = matrix.count, m = matrix.first?.count ?? 0
+    ///     - Space: O(1)
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        let n = matrix.count
+        guard let m = matrix.first?.count else { return false }
+        
+        var left = 0
+        var right = n - 1
+        var row = -1
+        while left <= right {
+            let mid = left + (right - left) / 2
+            if matrix[mid][0] <= target, matrix[mid][m - 1] >= target {
+                row = mid
+                break
+            } else if matrix[mid][0] > target {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        if row < 0 { return false }
+        left = 0
+        right = m - 1
+        while left <= right {
+            let mid = left + (right - left) / 2
+            if matrix[row][mid] == target { return true }
+            if matrix[row][mid] > target { right = mid - 1 }
+            else {left = mid + 1 }
+        }
+        return false 
+    }
+}
+
+class Solution {
+    /// - Complexity:
+    ///     - Time: O(log(m * n)), n = matrix.count, m = matrix.first?.count ?? 0
+    ///     - Space: O(1)
+    func searchMatrix2(_ matrix: [[Int]], _ target: Int) -> Bool {
+        let n = matrix.count
+        guard let m = matrix.first?.count else { return false }
+        var left = 0
+        var right = m * n - 1
+        while left <= right {
+            let mid = left + (right - left) / 2
+            let r = mid / m
+            let c = mid % m
+            if matrix[r][c] == target { return true }
+            if matrix[r][c] > target {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return false
+    }
+}
