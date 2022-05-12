2b3d8e9c864e17f0d3e49a604bea35196a59ca96
Yi <777705+antonio081014@users.noreply.github.com>
2022-05-12 15:12:58 -0700

    Update trapping-rain-water.swift

--- Swift/trapping-rain-water.swift
+++ Swift/trapping-rain-water.swift
@@ -44,3 +44,31 @@ class Solution {
         return result
     }
 }
+
+class Solution {
+    func trap(_ height: [Int]) -> Int {
+        var leftMax = 0
+        var rightMax = 0
+        var left = 0
+        var right = height.count - 1
+        var result = 0
+        while left < right {
+            if height[left] < height[right] {
+                if height[left] >= leftMax {
+                    leftMax = height[left]
+                } else {
+                    result += leftMax - height[left]
+                }
+                left += 1
+            } else {
+                if height[right] >= rightMax {
+                    rightMax = height[right]
+                } else {
+                    result += rightMax - height[right]
+                }
+                right -= 1
+            }
+        }
+        return result
+    }
+}
