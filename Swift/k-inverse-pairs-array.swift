a2db48746eb70c0eb9367519130a3f7c0ebafe7c
Yi <777705+antonio081014@users.noreply.github.com>
2022-07-18 16:17:43 -0700

    Update k-inverse-pairs-array.swift

--- Swift/k-inverse-pairs-array.swift
+++ Swift/k-inverse-pairs-array.swift
@@ -37,3 +37,23 @@ class Solution {
         return dp[n][k]
     }
 }
+
+// Mon Jul 18 16:17:35 PDT 2022
+class Solution {
+    func kInversePairs(_ n: Int, _ k: Int) -> Int {
+        let mod = 1000000007
+        var dp = Array(repeating: Array(repeating: 0, count: k + 1), count: n + 1)
+        for x in 1 ... n {
+            for y in 0 ... k {
+                if y == 0 {
+                    dp[x][y] = 1
+                } else {
+                    let val = (dp[x - 1][y] - (y >= x ? dp[x - 1][y - x]: 0) + mod) % mod
+                    dp[x][y] = (dp[x][y - 1] + val) % mod
+                }
+            }
+        }
+        // print(dp)
+        return (dp[n][k] + mod - (k > 0 ? dp[n][k - 1] : 0)) % mod
+    }
+}
